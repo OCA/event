6124675a582a23bde8a1cb759545dd0b641df82f
Antonio Espinosa <antespi@gmail.com>
2015-07-13 14:05:19 +0200

    Refactor using event_recalculate addon

--- event_project/models/event_event.py
+++ event_project/models/event_event.py
@@ -1,27 +1,5 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright :
-#        (c) 2014 Antiun Ingenieria, SL (Madrid, Spain, http://www.antiun.com)
-#                 Endika Iglesias <endikaig@antiun.com>
-#                 Antonio Espinosa <antonioea@antiun.com>
-#                 Javier Iniesta <javieria@antiun.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# See README.rst file on addon root folder for license details
 
 from openerp import models, fields, api
 
@@ -29,44 +7,55 @@ from openerp import models, fields, api
 class EventEvent(models.Model):
     _inherit = 'event.event'
 
-    project_template = fields.Many2one(
-        comodel_name='project.project', string='Template project',
-        domain="[('state', '=', 'template')]")
-    project = fields.Many2one(
+    project_id = fields.Many2one(
         comodel_name='project.project', string='Related project',
-        readonly=True)
+        oldname='project',
+        help="Project end date will be updated with event start date.")
+    task_ids = fields.One2many(
+        comodel_name='project.task', related='project_id.tasks',
+        string='Tasks', oldname='tasks')
+    work_ids = fields.One2many(
+        comodel_name='project.task.work', inverse_name='event_id',
+        string='Works')
+    count_tasks = fields.Integer(string='Task number', compute='_count_tasks')
 
-    def get_project_with_duplicate_template(self, template):
-        project_obj = self.env['project.project']
-        result = template.duplicate_template()
-        return project_obj.browse(int(result['res_id']))
+    @api.one
+    @api.depends('task_ids')
+    def _count_tasks(self):
+        self.count_tasks = len(self.task_ids)
+
+    def project_data_update(self, vals):
+        recalculate = False
+        if self.project_id:
+            project_vals = {}
+            if vals.get('name'):
+                project_vals['name'] = self.name
+            if vals.get('date_begin'):
+                project_vals['date'] = self.date_begin
+                recalculate = True
+            if vals.get('project_id'):
+                project_vals['event_id'] = self.id
+                recalculate = True
+            if project_vals:
+                self.project_id.write(project_vals)
+                return recalculate
+        return False
+
+    def project_free(self, vals):
+        if self.project_id and vals.get('project_id') is False:
+            self.project_id.write({'event_id': False})
 
     @api.model
     def create(self, vals):
         event = super(EventEvent, self).create(vals)
-        if (event.project_template and not event.project):
-            event.project = self.get_project_with_duplicate_template(
-                event.project_template)
-            event.project.reorganize_project(event, name=vals.get('name'))
+        event.project_data_update(vals)
         return event
 
     @api.one
     def write(self, vals):
-        project_obj = self.env['project.project']
-        project = None
-        date_begin = None
-        if vals.get('project_template') and not self.project:
-                project_template = project_obj.browse(
-                    int(vals['project_template']))
-                project = self.get_project_with_duplicate_template(
-                    project_template)
-                vals['project'] = project.id
-        else:
-            project = self.project
-        date_begin = fields.Datetime.from_string(
-            self.date_begin) if not vals.get(
-            'date_begin') else fields.Datetime.from_string(vals['date_begin'])
-        if date_begin:
-            project.reorganize_project(
-                self, date_begin=date_begin, name=vals.get('name'))
-        return super(EventEvent, self).write(vals)
+        self.project_free(vals)
+        super(EventEvent, self).write(vals)
+        recalculate = self.project_data_update(vals)
+        if recalculate and not self.env.context.get('no_recalculate'):
+            self.project_id.project_recalculate()
+        return True
