116cd21d1370a59bef4fad7afe23761323c3b8c9
Jairo Llopis <j.llopis@grupoesoc.es>
2015-08-10 13:36:46 +0200

    Add modules training, training_track and training_track_generate.

---
+++ training_track/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
+
+{
+    "name": "Training Tracks",
+    "version": "8.0.1.1.1",
+    "category": "Project",
+    "author": "Grupo ESOC",
+    "license": "AGPL-3",
+    "website": "http://www.grupoesoc.es",
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+    "summary": "Organize training tracks",
+    "depends": [
+        "training",
+        "website_event_track",
+    ],
+    "data": [
+        "views/event_track.xml",
+    ],
+    "demo": [
+        "demo/event.track.csv",
+    ],
+}
---
+++ training_track/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
+
+from openerp import fields, models
+
+
+class EventTrack(models.Model):
+    """Expand event tracks with training duration types.
+
+    This is used to calculate how many hours of each type have been fulfilled.
+    """
+
+    _inherit = "event.track"
+
+    duration_type_id = fields.Many2one(
+        "training.duration_type",
+        "Training hour type",
+        help="Training hour type of this track, if it belongs to a training "
+             "group.")
