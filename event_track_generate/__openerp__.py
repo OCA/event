9565852bf8095269f162b1fe41bb23aa039ddf3f
Jairo Llopis <j.llopis@grupoesoc.es>
2015-08-10 13:06:22 +0200

    Add module event_track_generate.

---
+++ event_track_generate/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
+
+{
+    "name": "Generate Event Tracks",
+    "version": "8.0.2.0.0",
+    "category": "Project",
+    "author": "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "http://www.grupoesoc.es",
+    "summary": "Insert automatically event tracks",
+    "depends": [
+        "website_event_track",
+    ],
+    "data": [
+        "views/event.xml",
+    ],
+}
