e63ce5a787989b164bdb26f0fac8f87208c71439
Antonio Espinosa <antespi@gmail.com>
2015-08-25 17:36:58 +0200

    [FIX] Set fields string and remove attended list, it is not neccesary

--- partner_event/models/res_partner.py
+++ partner_event/models/res_partner.py
@@ -11,42 +11,14 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     registrations = fields.One2many(
+        string="Event registrations",
         comodel_name='event.registration', inverse_name="partner_id")
     registration_count = fields.Integer(
-        string='Registration number', compute='_count_registration',
+        string='Event registrations number', compute='_count_registration',
         store=True)
-    attended_registrations = fields.One2many(
-        comodel_name='event.registration',
-        compute="_get_attended_registrations",
-        search="_search_attended_registrations")
     attended_registration_count = fields.Integer(
-        string='Attended registration number',
+        string='Event attended registrations number',
         compute='_count_attended_registration', store=True)
-    event_registrations = fields.One2many(
-        comodel_name="event.registration",
-        inverse_name="partner_id", string="Event registrations")
-
-    @api.one
-    @api.depends('registrations')
-    def _get_attended_registrations(self):
-        self.attended_registrations = self.registrations.filtered(
-            lambda x: x.state == 'done')
-
-    def _search_attended_registrations(self, operator, value):
-        registration_obj = self.env['event.registration']
-        domain = [('state', '=', 'done')]
-        if value is not False:
-            domain.append((registration_obj._rec_name, operator, value))
-        vals = registration_obj.read_group(
-            domain, ['partner_id'], ['partner_id'])
-        ids = [x['partner_id'][0] for x in vals if x['partner_id']]
-        cond = operator in NEGATIVE_TERM_OPERATORS
-        if value is False:
-            cond = not cond
-        if cond:
-            return [('id', 'not in', ids)]
-        else:
-            return [('id', 'in', ids)]
 
     @api.one
     @api.depends('registrations')
@@ -56,4 +28,5 @@ class ResPartner(models.Model):
     @api.one
     @api.depends('attended_registrations')
     def _count_attended_registration(self):
-        self.attended_registration_count = len(self.attended_registrations)
+        self.attended_registration_count = len(self.registrations.filtered(
+            lambda x: x.state == 'done'))
