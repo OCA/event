5f2471c8134bd576dccc855dae21f0b1d9c1c5cb
Jairo Llopis <j.llopis@grupoesoc.es>
2015-10-27 13:51:45 +0100

    Enable attendance monitoring. Add tests.

--- training_track/models/event_track.py
+++ training_track/models/event_track.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 # © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class EventTrack(models.Model):
@@ -17,3 +17,23 @@ class EventTrack(models.Model):
         "Training hour type",
         help="Training hour type of this track, if it belongs to a training "
              "group.")
+
+    monitor_attendance = fields.Boolean(
+        help="Should this track use attendance monitoring?")
+
+    attendee_ids = fields.Many2many(
+        "event.registration",
+        string="Attendees",
+        domain="[('event_id', '=', event_id)]",
+        help="People that attended this track.")
+
+    @api.constrains("attendee_ids", "event_id")
+    def _check_attendees_events(self):
+        """Enforce same event in registration and track."""
+        self.mapped("attendee_ids")._check_attended_tracks_events()
+
+    @api.onchange("duration_type_id")
+    def _onchange_monitor_attendance(self):
+        """Set attendance monitoring."""
+        for s in self:
+            s.monitor_attendance = s.duration_type_id.monitor_attendance
--- training_track/tests/__init__.py
+++ training_track/tests/__init__.py
@@ -2,4 +2,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 # © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
 
-from . import test_track
+from . import test_attendance_monitoring, test_track
