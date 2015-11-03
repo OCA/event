5e2766ea1b4e91cca6249fa09a9b6e3139dedb09
Javier Iniesta <javieria@antiun.com>
2015-11-03 17:40:48 +0100

    Auto create partners in event registrations

--- partner_event/models/event_event.py
+++ partner_event/models/event_event.py
@@ -3,6 +3,11 @@
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
 
-from . import event_event
-from . import event_registration
-from . import res_partner
+from openerp import models, fields
+
+
+class EventEvent(models.Model):
+    _inherit = 'event.event'
+
+    create_partner = fields.Boolean(string="Create Partners in registration",
+                                    default=False)
---
+++ partner_event/tests/test_event_registration.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3: Antiun Ingenieria S.L. - Javier Iniesta
+# See README.rst file on addon root folder for more details
+
+from openerp.tests.common import TransactionCase
+
+
+class TestEventRegistration(TransactionCase):
+
+    def setUp(self):
+        super(TestEventRegistration, self).setUp()
+        self.event_0 = self.env.ref('event.event_0')
+        self.event_0.create_partner = True
+        registration_model = self.env['event.registration']
+        partner_model = self.env['res.partner']
+        self.partner_01 = partner_model.create({'name': 'Test Partner 01',
+                                                'email': 'email01@test.com'})
+        self.registration_01 = registration_model.create({
+            'email': 'email01@test.com', 'event_id': self.event_0.id})
+        self.registration_02 = registration_model.create({
+            'email': 'email02@test.com', 'event_id': self.event_0.id,
+            'name': 'Test Registration 02', 'phone': '254728911'})
+
+    def test_create(self):
+        self.assertEqual(self.partner_01.name, self.registration_01.name)
+        self.assertEqual(self.partner_01.email, self.registration_01.email)
+        self.assertEqual(self.partner_01.phone, self.registration_01.phone)
+        partner_02 = self.registration_02.partner_id
+        self.assertEqual(partner_02.name, self.registration_02.name)
+        self.assertEqual(partner_02.email, self.registration_02.email)
+        self.assertEqual(partner_02.phone, self.registration_02.phone)
+
+    def test_count_registrations(self):
+        event_1 = self.env.ref('event.event_1')
+        registration_model = self.env['event.registration']
+        registration_03 = registration_model.create({
+            'event_id': event_1.id, 'partner_id': self.partner_01.id})
+        self.assertEqual(self.partner_01.registration_count, 2)
+        self.assertEqual(self.partner_01.attended_registration_count, 0)
+        registration_03.state = 'done'
+        self.assertEqual(self.partner_01.attended_registration_count, 1)
+
+    def test_button_register(self):
+        event_1 = self.env.ref('event.event_1')
+        wizard = self.env['res.partner.register.event'].create({
+            'event': event_1.id})
+        active_ids = [self.partner_01.id, self.registration_02.partner_id.id]
+        wizard.with_context({'active_ids': active_ids}).button_register()
