f1310f7b8074615b604e3a3b04851649a235628f
Jairo Llopis <yajo.sk8@gmail.com>
2015-11-04 17:54:13 +0100

    Add module website_event_sale_legal.

---
+++ website_event_sale_legal/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería, S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Legal terms per event",
+    "summary": "Make attendees to accept legal terms per event",
+    "version": "8.0.1.0.0",
+    "category": "Marketing",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería, S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "website_event_sale",
+        "website_sale_product_legal",
+    ],
+    "data": [
+        "views/event_event_view.xml",
+        "views/templates.xml",
+    ],
+}
