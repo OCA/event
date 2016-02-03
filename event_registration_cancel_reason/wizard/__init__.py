0736f5308543cd93d0ec98b09ac89ee0b4ecbec5
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-02-03 08:59:04 +0100

    [ADD] event_registration_cancel_reason
    =============================================
    Reasons for event registrations cancellations
    =============================================
    
    This module allows to add a predefined reason why a registration for an event
    is cancelled.
    
    Configuration
    =============
    
    Go to *Marketing > Configuration > Events > Registrations cancel reasons* for
    setting the possible reasons you want to handle. You can also set if a reason
    is only available for certain events or, if you leave it empty, for all.
    
    Usage
    =====
    
    Go to *Marketing > Events > Events*, enter into the details or any of the
    events, and press the red circle button in the right part of any of the
    registrations. A new screen will appear asking you to enter the reason why the
    registration is cancelled.
    
    You will get also the same screen when pressing the button
    *Cancel registration* from the details view of a registration in
    *Marketing > Events > Registrations*. On this object, you can group by the
    cancel reason in the list, calendar or graph view.

---
+++ event_registration_cancel_reason/wizard/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L.
+# © 2016 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import event_registration_cancel_log_reason
