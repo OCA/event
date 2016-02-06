3f4d5cedd5aadb41441c9b462dd4198c5f05059e
Andhitia Rama <andhitia.r@gmail.com>
2016-02-06 18:14:17 +0700

    website_event_share

---
+++ website_event_share/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Event Share',
+    'version': '8.0.1.0.0',
+    'summary': 'Add contacts to event',
+    'author': 'OpenSynergy Indonesia,Odoo Community Association (OCA)',
+    'website': 'https://opensynergy-indonesia.com',
+    'category': 'Marketing',
+    'depends': ['website_event'],
+    'data': ['views/website_event.xml'],
+    'installable': True,
+    'license': 'AGPL-3',
+}
