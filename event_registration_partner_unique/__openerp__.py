b81fcf35b58077733c08bd4c4785da6f442eeff1
Jairo Llopis <yajo.sk8@gmail.com>
2016-02-26 18:30:50 +0100

    Unique partners in registrations.
    This module allows you to avoid having a partner registrated twice in the same event.

---
+++ event_registration_partner_unique/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Unique Partner per Event",
+    "summary": "Enforces 1 registration per partner and event",
+    "version": "8.0.1.0.0",
+    "category": "Events",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "pre_init_hook": "pre_init_hook",
+    "depends": [
+        "event",
+    ],
+}
