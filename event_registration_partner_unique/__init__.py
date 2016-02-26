b984a47b7215c19998fdf78bf56c5ccec9cac652
Jairo Llopis <yajo.sk8@gmail.com>
2016-02-26 18:30:50 +0100

    [8.0][event_registration_partner_unique] Unique partners in registrations.
    This module allows you to avoid having a partner registrated twice in the same event.

---
+++ event_registration_partner_unique/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
+from .hooks import pre_init_hook
