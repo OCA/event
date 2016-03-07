d4a0c6f2524c9e3497aec6ae06d5e0a348c12589
Jairo Llopis <yajo.sk8@gmail.com>
2016-03-07 18:08:30 +0100

    Refactor module.
    - Remove dependency on event_sale.
    - Add custom field that allows to choose in which events to disable duplicates.
    - Add tests.
    - Add custom exception to be used in upcoming submodule (for event_sale).

---
+++ event_registration_partner_unique/exceptions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import _, exceptions
+
+
+class UniqueRegistrationPartnerValidationError(exceptions.ValidationError):
+    """Base class for this module's validation errors."""
+    def __init__(self, *args, **kwargs):
+        self._args, self._kwargs = args, kwargs
+        value = self._message()
+        super(UniqueRegistrationPartnerValidationError, self).__init__(value)
+
+    def _message(self):
+        """Format the message."""
+        return self.__doc__.format(*self._args, **self._kwargs)
+
+
+class DuplicatedPartnerError(UniqueRegistrationPartnerValidationError):
+    __doc__ = _("Duplicated partners found in event {0}: {1}.")
