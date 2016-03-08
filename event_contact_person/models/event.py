48cacd5ffa598ca9d4e245e8e6b4fae6c09af099
Michael Viriyananda <viriyananda.michael@gmail.com>
2016-03-08 10:12:04 +0700

    Change URL for all of headers

--- event_contact_person/models/event.py
+++ event_contact_person/models/event.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2016 Michael Viriyananda
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import fields, models
 
