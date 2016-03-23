1dedc1198b692572d1536ed25200be0855092f16
Jairo Llopis <yajo.sk8@gmail.com>
2016-03-23 17:41:42 +0100

    [8.0][website_event_filter_selector] Filter events with selectors.
    You can use selectors above the event list in the website, instead of the left column that there is right now.

---
+++ website_event_filter_selector/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Event Selection Filters",
+    "summary": "Add a customizable top area to filter events with selectors",
+    "version": "8.0.1.0.0",
+    "category": "Website",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_event",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "images": [
+        "images/all.png",
+        "images/choose.png",
+    ],
+}
