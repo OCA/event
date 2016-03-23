347fc4a5b201d7dafbdf3bf4dbbb0cd98899da45
Jairo Llopis <yajo.sk8@gmail.com>
2016-03-23 17:41:42 +0100

    [8.0][website_event_filter_selector] Filter events with selectors.
    You can use selectors above the event list in the website, instead of the left column that there is right now.
    
    Store user customizations in separate template.
    
    Acknowledge that you cannot hide only the date filter.
    
    Remove search button; search on selection change.
    
    Fix bug where date did not match what was being filtered.
    
    [FIX][8.0][website_event_filter_selector] Do not die without types or countries
    
    This basically fixes #44 and a similar bug detected that would happen when no countries were found.
    
    It just adds a `t-if` to avoid possible failures, [borrowing the idea from core `website_event` module](https://github.com/odoo/odoo/blob/8.0/addons/website_event/views/website_event.xml#L167).
    
    [8.0][IMP][website_event_filter_selector] Add city filter for events.
    
    Since this includes new logic, it adds also a template for left-column filters.
    
    With this patch, you can now filter events depending on their cities, not only on their countries.
    
    This also includes some modifications that will make filters occupy always a full bootstrap row, no matter if there are 1, 2, 3 or 4 filters, to avoid ugly layouts.

---
+++ website_event_filter_city/controllers/main.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import _, http
+from openerp.addons.website_event.controllers.main import website_event
+
+
+class WebsiteEvent(website_event):
+    @http.route()
+    def events(self, page=1, **searches):
+        # Get current render values
+        searches.setdefault("city", "all")
+        values = super(WebsiteEvent, self).events(page, **searches).qcontext
+        searches = values["searches"]
+
+        # Regenerate current domain. Ideally, upstream would make all this in a
+        # separate method and make our life easier, but not happening now.
+        domain = [("state", "in", ("draft", "confirm", "done"))]
+
+        # Date domain
+        if values["current_date"]:
+            for date in values["dates"]:
+                if values["current_date"] == date[1]:
+                    domain += date[2]
+                    break
+
+        # Type domain
+        if values["current_type"]:
+            domain.append(("type", "=", values["current_type"].id))
+
+        # Country domain
+        if values["current_country"]:
+            domain.append(("country_id", "=", values["current_country"].id))
+        elif searches["country"] == 'online':
+            domain.append(("country_id", "=", False))
+
+        # Handle city search
+        event_obj = http.request.env["event.event"]
+        cities = event_obj.read_group(
+            domain,
+            ["city"],
+            groupby="city",
+            orderby="city")
+        cities.insert(0, {"city_count": len(values["event_ids"]),
+                          "city": _("All Cities"),
+                          "key": "all"})
+        if searches["city"] != "all":
+            domain.append(("city", "=", searches["city"]))
+        values["cities"] = cities
+        values["current_city"] = searches["city"]
+
+        # We need a new pager now
+        step = 10  # This is hardcoded upstream too
+        values["pager"] = http.request.website.pager(
+            url="/event",
+            url_args={
+                "date": searches.get("date"),
+                "type": searches.get("type"),
+                "country": searches.get("country"),
+                "city": searches.get("city"),
+            },
+            total=event_obj.search(domain, count=True),
+            page=page,
+            step=step,  # This is hardcoded upstream too
+            scope=5)
+
+        # Return new event results
+        order = 'website_published desc, date_begin'
+        if searches["date"] == "old":
+            order += " desc"
+        values["event_ids"] = event_obj.search(
+            domain,
+            limit=step,
+            offset=values["pager"]["offset"],
+            order=order)
+
+        # Render template again
+        return http.request.website.render("website_event.index", values)
