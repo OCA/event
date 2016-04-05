0312cabb0cea8f5458c67e8b5040e9b7ff5d3ea2
Jairo Llopis <yajo.sk8@gmail.com>
2016-04-05 17:53:53 +0200

    Custom description for event types.
    It gets displayed in the frontend, and you can edit it through backend or frontend.

---
+++ website_event_type_description/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Event Type Description in Website",
+    "summary": "Display a specific description for each event type",
+    "version": "8.0.1.0.0",
+    "category": "Website",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_event",
+    ],
+    "data": [
+        "views/event_type_view.xml",
+        "views/event.xml",
+    ],
+    "images": [
+        "images/seminars.png",
+        "images/all.png",
+    ],
+}
