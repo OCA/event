14e2b668c0794907a6595dd1ba229c844abf73b1
Jairo Llopis <yajo.sk8@gmail.com>
2016-04-06 17:37:32 +0200

    [IMP][event_contact] Add contact_ids to event.type.
    Also it will load the contacts from the event type if the event has none and the type has some.

--- event_contact/models/event.py
+++ event_contact/models/event.py
@@ -2,12 +2,29 @@
 # © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
-class event_event(models.Model):
+class EventEvent(models.Model):
     _inherit = 'event.event'
 
     contact_ids = fields.Many2many(
         string='Contacts',
-        comodel_name='res.partner')
+        comodel_name='res.partner',
+        help='Partners available to attend attendees requests for this event.')
+
+    @api.multi
+    @api.onchange("type")
+    def _onchange_type_set_contact_ids(self):
+        if self.type.contact_ids and not self.contact_ids:
+            self.contact_ids = self.type.contact_ids
+
+
+class EventType(models.Model):
+    _inherit = 'event.type'
+
+    contact_ids = fields.Many2many(
+        string='Contacts',
+        comodel_name='res.partner',
+        help='Partners available to attend attendees requests by default for '
+             'events of this type.')
