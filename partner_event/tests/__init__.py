83ec513e33f179d89d6e6cc998673031b7653f4b
Antonio Espinosa <antespi@gmail.com>
2016-11-11 18:10:59 +0100

    Propagate partner changes and show errors when registering partners (#48)

--- partner_event/tests/__init__.py
+++ partner_event/tests/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Javier Iniesta
-# See README.rst file on addon root folder for more details
+# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2015 Antiun Ingenieria S.L. - Javier Iniesta
+# © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_event_registration
--- partner_event/wizard/res_partner_register_event.py
+++ partner_event/wizard/res_partner_register_event.py
@@ -1,7 +1,8 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2015 Antiun Ingenieria S.L. - Javier Iniesta
+# © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api
 
@@ -10,6 +11,7 @@ class ResPartnerRegisterEvent(models.TransientModel):
     _name = 'res.partner.register.event'
 
     event = fields.Many2one('event.event', required=True)
+    errors = fields.Text(readonly=True)
 
     def _prepare_registration(self, partner):
         return {
@@ -26,6 +28,25 @@ class ResPartnerRegisterEvent(models.TransientModel):
     def button_register(self):
         registration_obj = self.env['event.registration']
         partner_obj = self.env['res.partner']
+        errors = []
         for partner_id in self.env.context.get('active_ids', []):
             partner = partner_obj.browse(partner_id)
-            registration_obj.create(self._prepare_registration(partner))
+            try:
+                with self.env.cr.savepoint():
+                    registration_obj.create(
+                        self._prepare_registration(partner))
+            except:
+                errors.append(partner.name)
+        if errors:
+            self.errors = '\n'.join(errors)
+            data_obj = self.env.ref('partner_event.'
+                                    'res_partner_register_event_view')
+            return {
+                'type': 'ir.actions.act_window',
+                'res_model': self._name,
+                'view_mode': 'form',
+                'view_type': 'form',
+                'view_id': [data_obj.id],
+                'res_id': self.id,
+                'target': 'new',
+            }
