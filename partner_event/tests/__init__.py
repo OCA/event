e01b4a69955037e5073effe213b20570da246bac
cubells <vicent@vcubells.net>
2016-11-12 19:34:31 +0100

    [MIG] partner_event: Migration to 9.0

--- partner_event/tests/__init__.py
+++ partner_event/tests/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# © 2015 Antiun Ingenieria S.L. - Javier Iniesta
-# © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
+# © 2014 Tecnativa S.L. - Pedro M. Baeza
+# © 2015 Tecnativa S.L. - Javier Iniesta
+# © 2016 Tecnativa S.L. - Antonio Espinosa
+# © 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_event_registration
