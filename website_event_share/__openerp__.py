47b72da566b9229164e343115109a1baab3f9513
cubells <vicent@vcubells.net>
2016-11-28 12:58:33 +0100

    website_event_share module * Updated README.rst

--- website_event_share/__openerp__.py
+++ website_event_share/__openerp__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2016 OpenSynergy Indonesia
+# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Event Share',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'summary': 'Add contacts to event',
-    'author': 'OpenSynergy Indonesia,Odoo Community Association (OCA)',
+    'author': 'OpenSynergy Indonesia,'
+              'Tecnativa, '
+              'Odoo Community Association (OCA)',
     'website': 'https://opensynergy-indonesia.com',
     'category': 'Marketing',
     'depends': ['website_event'],
