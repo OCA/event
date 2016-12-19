c7d748595e84d0dffcada0089dc1a06e79f12fbb
Jairo Llopis <yajo.sk8@gmail.com>
2016-12-19 14:30:45 +0100

    [9.0][website_event_filter_selector] Migrate.
    Relicensed to LGPL, new JS API.
    
    Add test. Fix errors found with it.
    
    Go 100% coverage. Support online events.
    
    [FIX][website_event_filter_selector] Fix Travis build
    
    Builds failed on 2017-03-31 because the tour was expecting to see USA event starting in this month, but next day was another month.
    
    Well, in short, this fix makes the tour work on any day.

--- website_event_filter_city/models/event.py
+++ website_event_filter_city/models/event.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from openerp import fields, models
 
