4eff4b28848bcea3c44d2b7b4f4bb94c43145c85
cubells <vicent@vcubells.net>
2017-01-12 12:26:21 +0100

    [9.0][MIG] event_registration_cancel_reason module - Updated README - Updated views - Updated tests - Improve tests to achieve full test coverage

--- event_registration_cancel_reason/models/event_registration.py
+++ event_registration_cancel_reason/models/event_registration.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antiun Ingeniería S.L.
-# © 2016 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# Copyright 2016 Antiun Ingeniería S.L.
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import api, fields, models
