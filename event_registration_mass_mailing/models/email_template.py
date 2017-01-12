9dcd9452c90bdc39db35e95a465d91861bbee8a0
cubells <vicent@vcubells.net>
2017-01-12 20:07:06 +0100

    event_registration_mass_mailing module - Updated README - Updated views

--- event_registration_mass_mailing/models/email_template.py
+++ event_registration_mass_mailing/models/email_template.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Javier Iniesta
-# See README.rst file on addon root folder for more details
+# Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
+# Copyright 2017 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from lxml import etree
 
@@ -9,7 +10,7 @@ from openerp.tools.safe_eval import safe_eval
 
 
 class EmailTemplate(models.Model):
-    _inherit = 'email.template'
+    _inherit = 'mail.template'
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form',
@@ -17,18 +18,20 @@ class EmailTemplate(models.Model):
         res = super(EmailTemplate, self).fields_view_get(
             view_id=view_id, view_type=view_type,
             toolbar=toolbar, submenu=submenu)
-        if 'arch' in res:
-            doc = etree.XML(res['arch'])
-            for node in doc.xpath("//field[@name='model_id']"):
-                domain = node.get('domain')
-                if domain:
-                    domain = safe_eval(domain)
-                    for n, crit in enumerate(domain):
-                        if crit[0] == 'model' and crit[1] == 'in':
-                            v = list(crit[2])
-                            if 'event.registration' not in v:
-                                v.append('event.registration')
-                                domain[n] = ('model', 'in', tuple(v))
-                    node.set('domain', str(domain))
-            res['arch'] = etree.tostring(doc)
+        if 'arch' not in res:
+            return res
+        doc = etree.XML(res['arch'])
+        for node in doc.xpath("//field[@name='model_id']"):
+            domain = node.get('domain')
+            if not domain:
+                continue
+            domain = safe_eval(domain)
+            for n, crit in enumerate(domain):
+                if crit[0] == 'model' and crit[1] == 'in':
+                    v = list(crit[2])
+                    if 'event.registration' not in v:
+                        v.append('event.registration')
+                        domain[n] = ('model', 'in', tuple(v))
+            node.set('domain', str(domain))
+        res['arch'] = etree.tostring(doc)
         return res
