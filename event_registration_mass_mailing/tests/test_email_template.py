8cd4dd7e223ff65836b26a1d2361a9968b565a62
cubells <vicent@vcubells.net>
2017-01-12 20:07:06 +0100

    event_registration_mass_mailing module - Updated README - Updated views

--- event_registration_mass_mailing/tests/test_email_template.py
+++ event_registration_mass_mailing/tests/test_email_template.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Javier Iniesta
-# See README.rst file on addon root folder for more details
+# Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
+# Copyright 2017 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from lxml import etree
 
@@ -11,7 +12,7 @@ from openerp.tools.safe_eval import safe_eval
 class TestEmailTemplate(TransactionCase):
 
     def test_fields_view_get(self):
-        view = self.env['email.template'].with_context(
+        view = self.env['mail.template'].with_context(
             {'form_view_ref': 'mass_mailing.email_template_form_minimal'}
         ).fields_view_get()
         doc = etree.XML(view['arch'])
--- event_registration_mass_mailing/wizard/__init__.py
+++ event_registration_mass_mailing/wizard/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Javier Iniesta
-# See README.rst file on addon root folder for more details
+# Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import event_registration_mail_list_wizard
