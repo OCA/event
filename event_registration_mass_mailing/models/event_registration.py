9d000c51b58b5d788062c5590914c68d89535f42
cubells <vicent@vcubells.net>
2017-01-12 20:07:06 +0100

    event_registration_mass_mailing module - Updated README - Updated views

--- event_registration_mass_mailing/models/event_registration.py
+++ event_registration_mass_mailing/models/event_registration.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Javier Iniesta
-# See README.rst file on addon root folder for more details
+# Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields
 
