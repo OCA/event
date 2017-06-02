f219198626fdde11066b911dc1ba2087a0d47647
sergio.teruel <sergio.teruel@tecnativa.com>
2017-06-02 10:53:48 +0200

    [ADD] event_registration_multi_qty: Allow registering multiple quantities at once

---
+++ event_registration_multi_qty/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Sergio Teruel<sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Event Registration Multi Qty',
+    'version': '10.0.1.0.0',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    "license": "AGPL-3",
+    'website': 'https://www.tecnativa.com',
+    'category': 'Marketing',
+    'summary': 'Allow registration grouped by quantities',
+    'depends': ['event'],
+    'data': [
+        'views/event_view.xml',
+    ],
+    'installable': True,
+}
