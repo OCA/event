85900714a90a3b718df0112c41a6ee017e3e5ae7
David Vidal <chienandalu@gmail.com>
2017-06-14 17:27:56 +0200

    [MIG] event_registration_mass_mailing: Migration to 10.0

--- event_registration_mass_mailing/models/__init__.py
+++ event_registration_mass_mailing/models/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import email_template
 from . import event_registration
 from . import mass_mailing
--- event_registration_mass_mailing/models/mass_mailing.py
+++ event_registration_mass_mailing/models/mass_mailing.py
@@ -2,8 +2,7 @@
 # Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api
-from openerp.tools.translate import _
+from odoo import _, api, models
 
 
 class MassMailing(models.Model):
