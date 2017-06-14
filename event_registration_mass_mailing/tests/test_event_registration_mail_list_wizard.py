52bc3411cd8a83afdcb2b96fd6d884daa4c4233e
David Vidal <chienandalu@gmail.com>
2017-06-14 17:27:56 +0200

    [MIG] event_registration_mass_mailing: Migration to 10.0

--- event_registration_mass_mailing/tests/test_event_registration_mail_list_wizard.py
+++ event_registration_mass_mailing/tests/test_event_registration_mail_list_wizard.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestEventRegistrationMailListWizard(TransactionCase):
