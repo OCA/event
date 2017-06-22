05f99e3c5a8919fc6ebc7c32be8b016a6b48b171
Jairo Llopis <yajo.sk8@gmail.com>
2017-06-22 15:02:59 +0200

    [10.0][MIG][website_event_filter_selector]
    Includes update to use new tours api.
    
    Patch event counts when a city is searched.

--- website_event_filter_selector/models/event.py
+++ website_event_filter_selector/models/event.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class EventEvent(models.Model):
