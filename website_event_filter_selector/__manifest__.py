fc77f3f6ae014486d40247f5b76c729d936aa956
Jairo Llopis <yajo.sk8@gmail.com>
2017-06-22 15:02:59 +0200

    [10.0][MIG][website_event_filter_selector]
    Includes update to use new tours api.
    
    Patch event counts when a city is searched.

--- website_event_filter_selector/__manifest__.py
+++ website_event_filter_selector/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Website Event Selection Filters",
     "summary": "Add a customizable top area to filter events with selectors",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Website",
     "website": "https://www.tecnativa.com",
     "author": "Antiun Ingeniería S.L., "
