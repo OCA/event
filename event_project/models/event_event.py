73459c052d4a69e52aeb1ab9969be224792c5d33
David Vidal <chienandalu@gmail.com>
2017-06-23 16:58:11 +0200

    [MIG] event_project: Migration to 10.0

--- event_project/models/event_event.py
+++ event_project/models/event_event.py
@@ -1,32 +1,38 @@
 # -*- coding: utf-8 -*-
-# See README.rst file on addon root folder for license details
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import api, fields, models
 
 
 class EventEvent(models.Model):
     _inherit = 'event.event'
 
     project_id = fields.Many2one(
-        comodel_name='project.project', string='Related project',
-        oldname='project',
-        help="Project end date will be updated with event start date.")
+        comodel_name='project.project',
+        string='Related project',
+        help="Project end date will be updated with event start date.",
+    )
     task_ids = fields.One2many(
-        comodel_name='project.task', related='project_id.tasks',
-        string='Tasks', oldname='tasks')
-    work_ids = fields.One2many(
-        comodel_name='project.task.work', inverse_name='event_id',
-        string='Works')
-    count_tasks = fields.Integer(string='Task number', compute='_count_tasks')
+        comodel_name='project.task',
+        inverse_name='event_id',
+        string='Tasks',
+        readonly=True,
+    )
+    count_tasks = fields.Integer(
+        string='Task number',
+        compute='_count_tasks',
+    )
 
-    @api.one
+    @api.multi
     @api.depends('task_ids')
     def _count_tasks(self):
-        self.count_tasks = len(self.task_ids)
+        for event in self:
+            event.count_tasks = len(event.task_ids)
 
     def project_data_update(self, vals):
-        recalculate = False
         if self.project_id:
+            recalculate = False
             project_vals = {}
             if vals.get('name'):
                 project_vals['name'] = self.name
@@ -35,33 +41,47 @@ class EventEvent(models.Model):
                 recalculate = True
             if vals.get('project_id'):
                 project_vals['event_id'] = self.id
+                project_vals['calculation_type'] = 'date_end'
+                project_vals['date'] = self.date_begin
+                project_vals['name'] = self.name
                 recalculate = True
             if project_vals:
                 self.project_id.write(project_vals)
                 return recalculate
         return False
 
-    def project_free(self, vals):
-        if self.project_id and vals.get('project_id') is False:
-            self.project_id.write({'event_id': False})
-
     @api.model
     def create(self, vals):
+        if vals.get('project_id'):
+            vals['project_id'] = self.env[
+                'project.project'].browse(vals['project_id']).copy().id
         event = super(EventEvent, self).create(vals)
         event.project_data_update(vals)
         return event
 
-    @api.one
+    @api.multi
     def write(self, vals):
-        self.project_free(vals)
-        super(EventEvent, self).write(vals)
+        if self.project_id and not vals.get('project_id'):
+            self.project_id.event_id = False
+        if vals.get('project_id'):
+            vals['project_id'] = self.env[
+                'project.project'].browse(vals['project_id']).copy().id
+        res = super(EventEvent, self).write(vals)
         recalculate = self.project_data_update(vals)
         if recalculate and not self.env.context.get('no_recalculate'):
             self.project_id.project_recalculate()
-        return True
+        return res
 
     @api.multi
     def button_cancel(self):
-        """Cancel associated project when cancelling event."""
+        """Archive linked project when event is cancelled"""
         super(EventEvent, self).button_cancel()
-        self.mapped('project_id').set_cancel()
+        if self.project_id:
+            self.project_id.active = False
+
+    @api.multi
+    def button_draft(self):
+        """Uncharchive linked project when event is set to draft again"""
+        super(EventEvent, self).button_draft()
+        if self.project_id:
+            self.project_id.active = True
