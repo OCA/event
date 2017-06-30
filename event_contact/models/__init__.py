f9cc0913050aa119de2cfe6e4cba31d603dc567f
Jairo Llopis <yajo.sk8@gmail.com>
2017-06-30 10:39:10 +0200

    [MIG][event_contact] Migration to v10
    Document new behavior for event type contacts, and fix its test.

--- event_contact/models/__init__.py
+++ event_contact/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import event
+from . import event_event
+from . import event_type
