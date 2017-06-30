7a6d15685f6493fe53d322880f1f4995286ca276
Jairo Llopis <yajo.sk8@gmail.com>
2017-06-30 10:39:10 +0200

    [MIG][event_contact] Migration to v10
    Document new behavior for event type contacts, and fix its test.

--- event_contact/__manifest__.py
+++ event_contact/__manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-# © 2016 OpenSynergy Indonesia
+# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Event Contacts',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'summary': 'Add contacts to event and event type',
     'author': 'OpenSynergy Indonesia, '
-              'Antiun Ingeniería S.L., '
+              'Tecnativa, '
               'Odoo Community Association (OCA)',
     'website': 'https://opensynergy-indonesia.com',
     'category': 'Marketing',
