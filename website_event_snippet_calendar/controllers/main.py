d1892bc744d7ff7bf727be585c9a5521d9d36994
Jairo Llopis <yajo.sk8@gmail.com>
2018-01-17 11:24:28 +0000

    [ADD] website_event_snippet_calendar: Snippet for event calendar and list
    Allows the user to display a calendar combined with a list of events.
    
    Includes controllers for such information to be bundled into any page through an `<iframe>`.

---
+++ website_event_snippet_calendar/controllers/main.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Tecnativa - Jairo Llopis
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from datetime import date, timedelta
+from openerp.fields import Date
+from openerp.http import Controller, request, route
+
+
+class EventCalendar(Controller):
+    @route("/website_event_snippet_calendar/days_with_events",
+           auth="public", type="json")
+    def days_with_events(self, start, end):
+        """Let visitors know when are there going to be any events.
+
+        :param start string:
+            Search events from that date.
+
+        :param end string:
+            Search events until that date.
+        """
+        events = request.env["event.event"].search([
+            "|",
+            ("date_begin", "<", end),
+            ("date_end", ">", start),
+        ])
+        days = set()
+        one_day = timedelta(days=1)
+        for event in events:
+            now = Date.from_string(event.date_begin)
+            end = Date.from_string(event.date_end)
+            while now <= end:
+                days.add(now)
+                now += one_day
+        return [Date.to_string(day) for day in days]
+
+    @route("/website_event_snippet_calendar/events_for_day",
+           auth="public", type="json")
+    def events_for_day(self, day=None, limit=None):
+        """List events for a given day.
+
+        :param day string:
+            Date in a string. If ``None``, we'll search for upcoming events
+            from today up to specified limit.
+
+        :param limit int:
+            How many results to return.
+        """
+        ref = day or Date.to_string(date.today())
+        domain = [
+            ("date_end", ">=", ref),
+        ]
+        if day:
+            domain.append(("date_begin", "<=", ref))
+        return request.env["event.event"].search_read(
+            domain=domain,
+            limit=limit,
+            fields=[
+                "date_begin",
+                "name",
+                "event_type_id.display_name",
+                "website_published",
+            ],
+        )
+
+    @route("/website_event_snippet_calendar/embed",
+           auth="public", type="http", website=True)
+    def embed(self, *args, **kwargs):
+        return request.render("website_event_snippet_calendar.embed")
---
+++ website_event_snippet_calendar/static/src/js/animation.js
@@ -0,0 +1,166 @@
+/* Copyright 2018 Tecnativa - Jairo Llopis
+ * License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
+
+odoo.define('website_event_snippet_calendar.animation', function (require) {
+    "use strict";
+
+    var animation = require("web_editor.snippets.animation");
+    var core = require("web.core");
+    var time = require("web.time");
+    var ajax = require("web.ajax");
+
+    var DATE_FORMAT = time.strftime_to_moment_format("%Y-%m-%d");
+
+    var CalendarList = animation.Class.extend({
+        selector: ".s_event_calendar_list",
+
+        init: function () {
+            this.datepicker_options = {
+                inline: true,
+                minDate: moment().subtract(100, "years"),
+                maxDate: moment().add(100, "years"),
+                icons: {
+                    previous: "fa fa-chevron-left",
+                    next: "fa fa-chevron-right",
+                },
+                format: DATE_FORMAT,
+                useCurrent: false,
+            };
+            return this._super.apply(this, arguments);
+        },
+
+        start: function (editable_mode) {
+            if (editable_mode) {
+                return;
+            }
+            this._dates = {
+                min: null,
+                max: null,
+                matches: [],
+            };
+            this.$calendar = this.$(".s_event_calendar")
+                .on("dp.change", $.proxy(this, "day_selected"))
+                .on("dp.change", $.proxy(this, "highlight_events"))
+                .on("dp.update", $.proxy(this, "calendar_moved"));
+            this.$list = this.$(".s_event_list");
+            this.default_amount = Number(this.$(".js_amount").html()) || 4;
+            this.loaded_templates = ajax.loadXML(
+                "/website_event_snippet_calendar/static/src/xml/snippets.xml",
+                core.qweb
+            );
+            // Get initial events to render the list
+            this.load_events(null, this.default_amount)
+                .done($.proxy(this, "render_list"));
+            // Render the calendar and highlight it later
+            this.render_calendar();
+        },
+
+        day_before_show: function (date) {
+            var class_ = "",
+                date_str = time.date_to_str(date);
+            if (this._dates.matches.indexOf(date_str) !== -1) {
+                class_ = "ui-state-event";
+            }
+            return [true, class_, null];
+        },
+
+        day_selected: function (event) {
+            this.load_events(event.date.format(DATE_FORMAT))
+                .done($.proxy(this, "render_list"));
+        },
+
+        calendar_moved: function (event) {
+            if (event.change !== "M") {
+                // We only care when months are displayed
+                return;
+            }
+            // Preload dates if needed and show evented days
+            this.preload_dates(event.viewDate)
+                .done($.proxy(this, "highlight_events"));
+        },
+
+        highlight_events: function () {
+            var _dates = this._dates.matches;
+            this.$calendar.find(".day").filter(function () {
+                var day = moment(this.dataset.day, "L").format(DATE_FORMAT);
+                return _dates.indexOf(day) !== -1;
+            }).addClass("has-events");
+        },
+
+        preload_dates: function (when) {
+            var margin = moment.duration(4, "months");
+            // Don't preload if we have up to 4 months of margin
+            if (
+                this._dates.min && this._dates.max &&
+                this._dates.min <= when - margin &&
+                this._dates.max >= when + margin
+            ) {
+                return $.Deferred().resolve();
+            }
+            // Default values
+            margin.add(2, "months");
+            var start = moment(when - margin),
+                end = moment(when + margin);
+            // If we already preloaded, preload 6 more months
+            if (this._dates.min) {
+                start.subtract(6, "months");
+            }
+            if (this._dates.max) {
+                end.add(6, "months");
+            }
+            // Do the preloading
+            return this.load_dates(start, end);
+        },
+
+        load_dates: function (start, end) {
+            return ajax.rpc(
+                "/website_event_snippet_calendar/days_with_events",
+                {
+                    start: start.format(DATE_FORMAT),
+                    end: end.format(DATE_FORMAT),
+                }
+            )
+            .done($.proxy(this, "_update_dates_cache", start, end));
+        },
+
+        _update_dates_cache: function (start, end, dates) {
+            if (!this._dates.min || this._dates.min > start) {
+                this._dates.min = start;
+            }
+            if (!this._dates.max || this._dates.max < end) {
+                this._dates.max = end;
+            }
+            this._dates.matches = _.union(this._dates.matches, dates);
+        },
+
+        load_events: function (day, limit) {
+            return ajax.rpc(
+                "/website_event_snippet_calendar/events_for_day",
+                {day: day, limit: limit}
+            );
+        },
+
+        render_calendar: function () {
+            this.$calendar.empty().datetimepicker(this.datepicker_options);
+            this.calendar_moved({change: "M", viewDate: moment()});
+        },
+
+        render_list: function (events) {
+            this.loaded_templates.done($.proxy(this, "_render_list", events));
+        },
+        _render_list: function (events) {
+            this.$list.html(core.qweb.render(
+                "website_event_snippet_calendar.list",
+                {events: events}
+            ));
+        }
+    });
+
+    animation.registry.website_event_snippet_calendar = CalendarList;
+
+    return {
+        CalendarList: CalendarList,
+        DATE_FORMAT: DATE_FORMAT,
+    };
+
+});
