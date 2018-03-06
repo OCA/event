cc57c139d47c9036d87aeb1cf1c98cf929ea4101
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-03-06 13:52:24 +0100

    [IMP] event_email_reminder: Add the possibility to select recipients

--- event_email_reminder/models/event.py
+++ event_email_reminder/models/event.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
-# © 2016 Vicent Cubells <vicent.cubells@tecnativa.com>
+# Copyright 2016 Tecnativa - Sergio Teruel
+# Copyright 2016 Tecnativa - Vicent Cubells
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
@@ -11,16 +12,33 @@ class EventEvent(models.Model):
     _inherit = 'event.event'
 
     @api.model
+    def _send_event_template(self, events, template, partner_ids):
+        ctx = self.env.context.copy()
+        ctx.update({
+            'events': events.sorted(lambda x: x.date_begin),
+        })
+        for partner_id in partner_ids:
+            lang = self.env['res.partner'].browse(partner_id).lang
+            # Set the contexts with the partner's language
+            ctx.update({
+                'events': ctx['events'].with_context(lang=lang),
+                'lang': lang,
+            })
+            template.with_context(ctx).send_mail(partner_id)
+
+    @api.model
     def run_event_email_reminder(
             self, days=7, draft_events=False, near_events=False,
-            template_id=None):
+            template_id=None, partner_ids=None):
         """Enqueue mail with a summary of events that begin on days parameter
-        :param int days: number of days to reminder when events start
-        :param bool draft_events: filter by draft events too
-        :param bool near_events:
-            If you want receive the events which start between now and
-            limit date
-        :param str template_id: id of a template or None
+
+        :param int days: number of days to reminder when events start.
+        :param bool draft_events: filter by draft events too.
+        :param bool near_events: If you want receive the events which start
+          between now and limit date.
+        :param int template_id: id of a template or None.
+        :param list(int) partner_ids: list of IDs of the partners we
+          want to notify.
         """
         today = fields.Date.context_today(self)
         limit_date = fields.Date.from_string(today) + timedelta(days=days)
@@ -48,11 +66,12 @@ class EventEvent(models.Model):
                     'event_email_reminder.event_email_reminder_template')
             else:
                 template = self.env['mail.template'].browse(template_id)
-            for user in events.mapped('user_id'):
-                ctx = self.env.context.copy()
-                event_by_user = events.filtered(lambda x: x.user_id == user)
-                ctx.update({
-                    'events': event_by_user.sorted(lambda x: x.date_begin),
-                })
-                template.with_context(ctx).send_mail(user.partner_id.id)
+            if partner_ids:
+                self._send_event_template(events, template, partner_ids)
+            else:
+                for user in events.mapped('user_id'):
+                    events = events.filtered(lambda x: x.user_id == user)
+                    self._send_event_template(
+                        events, template, user.partner_id.ids,
+                    )
         return True
