e4b0f363c4d786e12324951795aeb7467ff0dd54
Jairo Llopis <yajo.sk8@gmail.com>
2018-03-29 13:57:54 +0100

    [FIX] event_registration_mass_mailing: Fix integration tests
    Testing after installing mass_mailing_list_dynamic produces:
    
        IntegrityError: null value in column "sync_domain" violates not-null constraint

--- event_registration_mass_mailing/__manifest__.py
+++ event_registration_mass_mailing/__manifest__.py
@@ -6,7 +6,7 @@
 {
     'name': "Mass mailing from events",
     'category': 'Marketing',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'depends': [
         'event',
         'mass_mailing'
