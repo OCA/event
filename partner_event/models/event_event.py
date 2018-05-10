5bfeb8dbdc7dfa744f741b0f0bc56880198d9d02
hveficent <hector.villarreal@eficent.com>
2018-05-10 12:15:56 +0200

    [MIG] partner_event: Migration to 11.0

--- partner_event/models/event_event.py
+++ partner_event/models/event_event.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# © 2014 Tecnativa S.L. - Pedro M. Baeza
-# © 2015 Tecnativa S.L. - Javier Iniesta
-# © 2016 Tecnativa S.L. - Antonio Espinosa
-# © 2016 Tecnativa S.L. - Vicent Cubells
+# Copyright 2014 Tecnativa S.L. - Pedro M. Baeza
+# Copyright 2015 Tecnativa S.L. - Javier Iniesta
+# Copyright 2016 Tecnativa S.L. - Antonio Espinosa
+# Copyright 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
