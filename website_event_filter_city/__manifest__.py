da59b8cddbe2d8126fa295c7bf267f3cb55c6ee7
tarteo <d.sluijk@onestein.nl>
2018-05-30 16:01:22 +0200

    [MIG] website_event_filter_selector: Migration to 11.0
    [IMP] Use new README approach
    
    [IMP] Removed the use of @class
    
    [FIX] Column classes
    
    [UPD] README.rst
    
    [UPD] README.rst

--- website_event_filter_city/__manifest__.py
+++ website_event_filter_city/__manifest__.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Website Event Selection Filters",
     "summary": "Add a customizable top area to filter events with selectors",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Website",
-    "website": "https://www.tecnativa.com",
+    "website": "https://github.com/OCA/event",
     "author": "Antiun Ingeniería S.L., "
               "Tecnativa, "
+              "Onestein, "
               "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
