6a8da8e35a6421f6218bb3f11ab75627b7a8f948
cristinamartinrod <cristina.marrod@gmail.com>
2018-10-30 17:19:12 +0100

    [11.0][MIG] event_registration_partner_unique: Migration to 11.0

--- event_registration_partner_unique/exceptions.py
+++ event_registration_partner_unique/exceptions.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# Copyright 2016 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, exceptions
+from odoo import _, exceptions
 
 
 class UniqueRegistrationPartnerValidationError(exceptions.ValidationError):
