069d03183e3dade2a6f7755f1ec8e7f7b05d04ae
cristinamartinrod <cristina.marrod@gmail.com>
2018-10-30 17:19:12 +0100

    [11.0][MIG] event_registration_partner_unique: Migration to 11.0

--- event_registration_partner_unique/__manifest__.py
+++ event_registration_partner_unique/__manifest__.py
@@ -1,21 +1,21 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# Copyright 2016 Tecnativa - Jairo Llopis
 # Copyright 2017 Tecnativa - Vicent Cubells
+# Copyright 2018 Tecnativa - Cristina Martin R.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Unique Partner per Event",
     "summary": "Enforces 1 registration per partner and event",
-    "version": "9.0.1.0.0",
-    "category": "Events",
-    "website": "http://www.antiun.com",
-    "author": "Antiun Ingeniería S.L., "
-              "Tecnativa, "
+    "version": "11.0.1.0.0",
+    "category": "Marketing",
+    "website": "https://github.com/oca/event",
+    "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": [
         "event",
+        "partner_event",
     ],
     "data": [
         "views/event_event_view.xml",
