ae0e444010965324b7321e36cde4d872bc44f8b3
David <david.vidal@tecnativa.com>
2019-03-21 10:20:09 +0100

    [12.0] event_registration_multi_qty: Migration to 12.0

--- event_registration_multi_qty/__manifest__.py
+++ event_registration_multi_qty/__manifest__.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Sergio Teruel<sergio.teruel@tecnativa.com>
+# Copyright 2017 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     'name': 'Event Registration Multi Qty',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
     "license": "AGPL-3",
-    'website': 'https://www.tecnativa.com',
+    'website': 'https://www.github.com/event',
     'category': 'Marketing',
     'summary': 'Allow registration grouped by quantities',
     'depends': ['event'],
