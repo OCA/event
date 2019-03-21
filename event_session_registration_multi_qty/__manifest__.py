48c6b37d088e799c315c186468486a52fb936fb5
David <david.vidal@tecnativa.com>
2019-03-21 12:53:04 +0100

    [12.0] event_session_registration_multi_qty: Migration to 12.0

--- event_session_registration_multi_qty/__manifest__.py
+++ event_session_registration_multi_qty/__manifest__.py
@@ -1,20 +1,18 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 David Vidal <david.vidal@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017-19 Tecnativa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
 {
     'name': 'Event Registration Multi Qty',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
     "license": "AGPL-3",
-    'website': 'https://www.tecnativa.com',
+    'website': 'https://www.github.com/OCA/event',
     'category': 'Marketing',
-    'summary': 'Allow registration grouped by quantities',
+    'summary': 'Allow registration grouped by quantities in sessions',
     'depends': [
         'event_session',
         'event_registration_multi_qty',
     ],
-    'data': [],
     'installable': True,
     'auto_install': True,
 }
---
+++ event_session_registration_multi_qty/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module allows to make registration to sessions with multiple attendees.
