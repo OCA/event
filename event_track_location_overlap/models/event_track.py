50edaf549d546ecafc76b86e132193d77973feb1
Jairo Llopis <yajo.sk8@gmail.com>
2019-04-16 13:56:27 +0100

    [MIG] event_track_location_overlap: Update data model
    The new data model in Odoo 12.0 replaces `state` for `stage_id`, which includes a flag that indicates if the stage is cancelled. Use it now.
    
    [UPD] README.rst
    
    [UPD] README.rst
    
    [UPD] Update event_track_location_overlap.pot

--- event_track_location_overlap/models/event_track.py
+++ event_track_location_overlap/models/event_track.py
@@ -8,7 +8,7 @@ class EventTrack(models.Model):
     _inherit = "event.track"
 
     @api.multi
-    @api.constrains("date", "duration", "location_id", "state")
+    @api.constrains("date", "duration", "location_id", "stage_id")
     def _check_location_overlap(self):
         """Make sure no location overlaps happen."""
         self.mapped("location_id")._check_overlappable()
