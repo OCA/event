1b38163fc668ef26f6c1ba1ad3bbb62b7187ab70
Jairo Llopis <yajo.sk8@gmail.com>
2019-04-18 12:45:54 +0100

    [MIG] event_email_reminder: Migrate to v12
    - Standard migration.
    - Use native datetime types.
    - Split README.
    - Faster tests using `SavepointCase`.
    
    [UPD] README.rst
    
    [UPD] Update event_email_reminder.pot

--- event_email_reminder/models/__init__.py
+++ event_email_reminder/models/__init__.py
@@ -1,6 +1,1 @@
-# -*- coding: utf-8 -*-
-# © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
-# © 2016 Vicent Cubells <vicent.cubells@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import event
