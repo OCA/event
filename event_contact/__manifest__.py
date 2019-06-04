b5195b3f5c0a1aa3206cd16377c33678846ad111
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-06-04 11:24:48 +0200

    [MIG] event_contact: Migration to 12.0

--- event_contact/__manifest__.py
+++ event_contact/__manifest__.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 OpenSynergy Indonesia
 # Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2019 Alexandre Díaz <alexandre.diaz@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Event Contacts',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'summary': 'Add contacts to event and event type',
     'author': 'OpenSynergy Indonesia, '
               'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'website': 'https://opensynergy-indonesia.com',
+    'website': 'https://github.com/OCA/event',
     'category': 'Marketing',
     'depends': ['event'],
     'data': [
