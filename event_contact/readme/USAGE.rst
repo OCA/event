db672e0d2cb71ebfcdf15fa111fe86699ef31b39
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-06-04 11:24:48 +0200

    [MIG] event_contact: Migration to 12.0

---
+++ event_contact/readme/USAGE.rst
@@ -0,0 +1,13 @@
+To set contacts to event, you need to:
+
+#. Go to *Events > Events*.
+#. Edit or create one.
+#. You will see a new field under field *Organizer* named *Contacts*.
+
+To set contacts for an event type, you need to:
+
+#. Go to *Events > Configuration > Event Categories*.
+#. Edit or create one.
+#. You will see a new field on *Attendees Section* named *Contacts*.
+#. After doing this, if you create an event and set it this type, the event
+   will get appended these contacts automatically.
--- event_contact/tests/test_event.py
+++ event_contact/tests/test_event.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2019 Alexandre Díaz <alexandre.diaz@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp.tests.common import TransactionCase
@@ -8,16 +8,35 @@ from openerp.tests.common import TransactionCase
 class EventCase(TransactionCase):
     def setUp(self, *args, **kwargs):
         super(EventCase, self).setUp(*args, **kwargs)
-        self.type1 = self.env.ref("event.event_type_1")
-        self.type2 = self.env.ref("event.event_type_2")
-        self.type1.contact_ids = (
-            self.env.ref("base.res_partner_1") |
-            self.env.ref("base.res_partner_2"))
-        self.type2.contact_ids = (
-            self.env.ref("base.res_partner_3") |
-            self.env.ref("base.res_partner_4"))
 
-        self.event1 = self.env.ref("event.event_1")
+        # Partners
+        self.partner1 = self.env['res.partner'].create({
+            'name': 'Test Partner 1',
+        })
+        self.partner2 = self.env['res.partner'].create({
+            'name': 'Test Partner 2',
+        })
+        self.partner3 = self.env['res.partner'].create({
+            'name': 'Test Partner 3',
+        })
+        self.partner4 = self.env['res.partner'].create({
+            'name': 'Test Partner 4',
+        })
+
+        self.type1 = self.env['event.type'].create({
+            'name': 'Event Type Test 1',
+            'contact_ids': [(6, False, [self.partner1.id, self.partner2.id])],
+        })
+        self.type2 = self.env['event.type'].create({
+            'name': 'Event Type Test 2',
+            'contact_ids': [(6, False, [self.partner3.id, self.partner4.id])],
+        })
+
+        self.event1 = self.env['event.event'].create({
+            'name': 'Event Test 1',
+            'date_begin': '2019-06-20',
+            'date_end': '2019-06-23',
+        })
 
     def test_event_onchange_type_contacts_empty(self):
         """You get default contacts from type."""
