d3bc33d175f82f1f75ab8a4a993cfb59bb895295
David <david.vidal@tecnativa.com>
2019-08-22 08:11:42 +0200

    [FIX] event_session: conflicting configuration

--- event_session/__manifest__.py
+++ event_session/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Event Sessions',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
     "license": "AGPL-3",
