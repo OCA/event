a983e70d02f117b767dea8a35cc48c8b9fc69741
Chris Mann <chrisandrewmann@gmail.com>
2019-09-01 16:53:45 +0100

    Add new module website_event_attendee_with_login, to require event attendee registrants to have their own accounts. All tickets will be under the name/email of the logged-in user preventing them from signing up members of the public. This is important for companies who only allow approved members to attend events such as webinars.

---
+++ website_event_attendee_with_login/models/event.py
@@ -0,0 +1,13 @@
+# Copyright 2019 Chris Mann - github.com/chrisandrewmann
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class EventEvent(models.Model):
+    _inherit = 'event.event'
+
+    website_attendee_require_login = fields.Boolean(
+        string='Require attendee login for tickets',
+        help='If set, an attendee must have their own account '
+             'to register for an event. All tickets limited to their logged-in name/email.',
+    )
---
+++ website_event_attendee_with_login/readme/CONFIGURE.rst
@@ -0,0 +1,4 @@
+
+To configure this module, you need to:
+
+#. Go to an event and set the Require attendee to have own login to register option on.
