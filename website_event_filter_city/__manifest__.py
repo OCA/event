588832bc1ed675f8e1c47632635d87b38590c0fa
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-04-24 09:45:18 +0100

    [IMP] website_event_filter_selector: make prettier
    Use `custom-select` in `<select>` elements instead of just `form-control`. This way BS4 can style the dropdowns more freely and they look more consistent with the rest of the site.
    
    I also delete a view that is no longer used.
    
    TT17632
    
    website_event_filter_selector 12.0.1.2.0

--- website_event_filter_city/__manifest__.py
+++ website_event_filter_city/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Website Event Selection Filters",
     "summary": "Add a customizable top area to filter events with selectors",
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.2.0",
     "category": "Website",
     "website": "https://github.com/OCA/event",
     "author": "Antiun Ingeniería S.L., "
