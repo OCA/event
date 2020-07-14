cc76f4c63183c12b806c7a0394ce5849adff0300
david <david.vidal@tecnativa.com>
2020-07-14 15:27:02 +0200

    [MIG] event_registration_mass_mailing: Migration to 12.0

---
+++ event_registration_mass_mailing/readme/USAGE.rst
@@ -0,0 +1,8 @@
+To use this module you need to:
+
+#. Go to an event attendees tree view and select the ones you want to put into
+   the mailing list.
+#. Click on *Action > Add to mailing list* and a wizard will pop up.
+#. Choose the mailing lists you want to put the registrations into.
+#. A new contact will be created for each one of the attendees if the mail
+   address wasn't in the list already.
--- event_registration_mass_mailing/wizard/event_registration_mail_list_wizard.py
+++ event_registration_mass_mailing/wizard/event_registration_mail_list_wizard.py
@@ -1,34 +1,36 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
+# Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class EventRegistrationMailListWizard(models.TransientModel):
     _name = "event.registration.mail.list.wizard"
     _description = "Create contact mailing list"
 
-    mail_list = fields.Many2one(comodel_name="mail.mass_mailing.list",
-                                string="Mailing list")
-    event_registrations = fields.Many2many(comodel_name="event.registration",
-                                           relation="mail_list_wizard_event"
-                                           "_registration")
+    mail_list = fields.Many2one(
+        comodel_name="mail.mass_mailing.list", string="Mailing list"
+    )
+    event_registrations = fields.Many2many(
+        comodel_name="event.registration",
+        relation="mail_list_wizard_event" "_registration",
+    )
 
-    @api.multi
     def add_to_mail_list(self):
-        contact_obj = self.env['mail.mass_mailing.contact']
-        registration_obj = self.env['event.registration']
-        for registration_id in self.env.context.get('active_ids', []):
-            registration = registration_obj.browse(registration_id)
-            criteria = [('email', '=', registration.email),
-                        ('list_id', '=', self.mail_list.id)]
-            contact_test = contact_obj.search(criteria)
-            if contact_test:
+        contact_obj = self.env["mail.mass_mailing.contact"]
+        for registration in self.env.context.get("active_ids", []):
+            registration = self.env["event.registration"].browse(registration)
+            if contact_obj.search(
+                [
+                    ("email", "=", registration.email),
+                    ("list_ids", "in", self.mail_list.id),
+                ]
+            ):
                 continue
-            contact_vals = {
-                'email': registration.email,
-                'name': registration.name,
-                'list_id': self.mail_list.id
-            }
-            contact_obj.create(contact_vals)
+            contact_obj.create(
+                {
+                    "email": registration.email,
+                    "name": registration.name,
+                    "list_ids": [[6, 0, [self.mail_list.id]]],
+                }
+            )
