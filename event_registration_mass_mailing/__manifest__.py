0bef16f82827fd90aaf394f9cf938c210e7d7d63
david <david.vidal@tecnativa.com>
2020-07-14 15:27:02 +0200

    [MIG] event_registration_mass_mailing: Migration to 12.0

--- event_registration_mass_mailing/__manifest__.py
+++ event_registration_mass_mailing/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
 # Copyright 2017 Tecnativa - Vicent Cubells
+# Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
-    'name': "Mass mailing from events",
+    'name': "Put event registrations emails into mailing lists",
     'category': 'Marketing',
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.0',
     'depends': [
         'event',
         'mass_mailing'
@@ -17,8 +16,8 @@
     ],
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'website': 'http://www.antiun.com',
+    'website': 'http://github.com/OCA/event',
     'license': 'AGPL-3',
     'installable': True,
-    'auto_install': True,
+    'auto_install': False,
 }
