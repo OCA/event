cb472987aeffb99056d295f06812a663bdef9496
david <david.vidal@tecnativa.com>
2020-07-14 15:27:02 +0200

    [MIG] event_registration_mass_mailing: Migration to 12.0

--- event_registration_mass_mailing/__init__.py
+++ event_registration_mass_mailing/__init__.py
@@ -1,6 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import models
 from . import wizard
--- event_registration_mass_mailing/wizard/__init__.py
+++ event_registration_mass_mailing/wizard/__init__.py
@@ -1,5 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Antiun Ingenieria S.L. - Javier Iniesta
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import event_registration_mail_list_wizard
