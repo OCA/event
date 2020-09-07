0016a32b46ac98fa196956d7e6c113165b4c78ba
Víctor Martínez <victor.martinez@tecnativa.com>
2020-09-07 11:23:55 +0200

    [IMP] : black, isort, prettier

--- event_registration_cancel_reason/models/event_registration.py
+++ event_registration_cancel_reason/models/event_registration.py
@@ -8,37 +8,42 @@ from openerp import api, fields, models
 
 
 class EventRegistration(models.Model):
-    _inherit = 'event.registration'
+    _inherit = "event.registration"
 
     cancel_reason_id = fields.Many2one(
-        comodel_name='event.registration.cancel.reason', readonly=True,
-        string="Cancellation Reason", ondelete="restrict")
+        comodel_name="event.registration.cancel.reason",
+        readonly=True,
+        string="Cancellation Reason",
+        ondelete="restrict",
+    )
 
     @api.multi
     def button_reg_cancel(self):
-        if self.env.context.get('bypass_reason'):
+        if self.env.context.get("bypass_reason"):
             return super(EventRegistration, self).button_reg_cancel()
         return {
-            'type': 'ir.actions.act_window',
-            'name': 'Cancellation reason',
-            'res_model': 'event.registration.cancel.log.reason',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'target': 'new',
+            "type": "ir.actions.act_window",
+            "name": "Cancellation reason",
+            "res_model": "event.registration.cancel.log.reason",
+            "view_type": "form",
+            "view_mode": "form",
+            "target": "new",
         }
 
     @api.multi
     def do_draft(self):
         super(EventRegistration, self).do_draft()
-        self.write({'cancel_reason_id': False})
+        self.write({"cancel_reason_id": False})
 
 
 class EventRegistrationCancelReason(models.Model):
-    _name = 'event.registration.cancel.reason'
-    _description = 'Event Registration Cancel Reason'
+    _name = "event.registration.cancel.reason"
+    _description = "Event Registration Cancel Reason"
 
-    name = fields.Char('Reason', required=True, translate=True)
+    name = fields.Char("Reason", required=True, translate=True)
     event_type_ids = fields.Many2many(
-        comodel_name="event.type", string="Event types",
+        comodel_name="event.type",
+        string="Event types",
         help="Select the event types where you want to use this cancellation "
-             "reason. Leave it empty for using in all.")
+        "reason. Leave it empty for using in all.",
+    )
