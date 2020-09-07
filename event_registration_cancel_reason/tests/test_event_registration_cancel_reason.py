60b5b30477487ae1aeb28d131df3579988b4e58f
Víctor Martínez <victor.martinez@tecnativa.com>
2020-09-07 11:23:55 +0200

    [IMP] : black, isort, prettier

--- event_registration_cancel_reason/tests/test_event_registration_cancel_reason.py
+++ event_registration_cancel_reason/tests/test_event_registration_cancel_reason.py
@@ -10,32 +10,35 @@ from odoo.tests import common
 class TestEventRegistrationCancelReason(common.TransactionCase):
     def setUp(self):
         super(TestEventRegistrationCancelReason, self).setUp()
-        self.event1 = self.env['event.event'].create(
-            {'name': 'Test event',
-             'event_type_id': self.env.ref('event.event_type_1').id,
-             'date_begin': fields.Date.today(),
-             'date_end': fields.Date.today()})
+        self.event1 = self.env["event.event"].create(
+            {
+                "name": "Test event",
+                "event_type_id": self.env.ref("event.event_type_1").id,
+                "date_begin": fields.Date.today(),
+                "date_end": fields.Date.today(),
+            }
+        )
         self.event2 = self.event1.copy()
-        self.partner = self.env['res.partner'].create({'name': 'Test partner'})
-        self.cancel_reason = self.env[
-            'event.registration.cancel.reason'].create({'name': 'Test reason'})
-        self.registration1 = self.env['event.registration'].create(
-            {'event_id': self.event1.id,
-             'partner_id': self.partner.id})
+        self.partner = self.env["res.partner"].create({"name": "Test partner"})
+        self.cancel_reason = self.env["event.registration.cancel.reason"].create(
+            {"name": "Test reason"}
+        )
+        self.registration1 = self.env["event.registration"].create(
+            {"event_id": self.event1.id, "partner_id": self.partner.id}
+        )
         self.registration2 = self.registration1.copy()
         self.registration2.event_id = self.event2
         self.registrations = self.registration1 | self.registration2
-        self.wizard_model = self.env['event.registration.cancel.log.reason']
+        self.wizard_model = self.env["event.registration.cancel.log.reason"]
 
     def test_cancel(self):
         action = self.registration1.button_reg_cancel()
-        self.assertEqual(action.get('type'), 'ir.actions.act_window')
+        self.assertEqual(action.get("type"), "ir.actions.act_window")
         wizard = self.wizard_model.with_context(
-            active_ids=self.registrations.ids).create(
-            {'reason_id': self.cancel_reason.id})
+            active_ids=self.registrations.ids
+        ).create({"reason_id": self.cancel_reason.id})
         wizard.button_log()
-        self.assertEqual(
-            self.registration1.cancel_reason_id, self.cancel_reason)
+        self.assertEqual(self.registration1.cancel_reason_id, self.cancel_reason)
         self.registration1.do_draft()
         self.assertFalse(self.registration1.cancel_reason_id)
 
@@ -43,14 +46,14 @@ class TestEventRegistrationCancelReason(common.TransactionCase):
         """Registration cancel from different event types are aborted."""
         self.event2.event_type_id = self.env.ref("event.event_type_2")
         with self.assertRaises(exceptions.ValidationError):
-            self.wizard_model.with_context(
-                active_ids=self.registrations.ids).create(
-                {'reason_id': self.cancel_reason.id})
+            self.wizard_model.with_context(active_ids=self.registrations.ids).create(
+                {"reason_id": self.cancel_reason.id}
+            )
 
     def test_cancel_one_event_without_type(self):
         """Registration cancel from 2 events (1 typed, 1 not) are aborted."""
         self.event2.event_type_id = False
         with self.assertRaises(exceptions.ValidationError):
-            self.wizard_model.with_context(
-                active_ids=self.registrations.ids).create(
-                {'reason_id': self.cancel_reason.id})
+            self.wizard_model.with_context(active_ids=self.registrations.ids).create(
+                {"reason_id": self.cancel_reason.id}
+            )
