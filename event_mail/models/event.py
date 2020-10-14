d7a1e0dbfe3c092829cbe08f6b7b3baa4b0eff81
Pablo Fuentes <pablo@studio73.es>
2020-10-14 16:50:37 +0200

    [IMP] event_mail: black,flake8,prettier

--- event_mail/models/event.py
+++ event_mail/models/event.py
@@ -4,34 +4,39 @@ from odoo import api, fields, models
 
 
 class EventEvent(models.Model):
-    _inherit = 'event.event'
+    _inherit = "event.event"
 
     @api.model
     def _default_event_mail_template_id(self):
-        company = self.env['res.company']._company_default_get('event.event')
+        company = self.env["res.company"]._company_default_get("event.event")
         return company.event_mail_template_id
 
     event_mail_template_id = fields.Many2one(
-        comodel_name='event.mail.template',
-        string='Mail Template Scheduler',
+        comodel_name="event.mail.template",
+        string="Mail Template Scheduler",
         default=_default_event_mail_template_id,
     )
 
-    @api.onchange('event_mail_template_id')
+    @api.onchange("event_mail_template_id")
     def _onchange_event_mail_template_id(self):
         vals = [(6, 0, [])]
         if self.event_mail_template_id.exists():
-            for scheduler in \
-                    self.event_mail_template_id.scheduler_template_ids:
-                vals.append((0, 0, {
-                    'interval_nbr': scheduler.interval_nbr,
-                    'interval_unit': scheduler.interval_unit,
-                    'interval_type': scheduler.interval_type,
-                    'template_id': scheduler.template_id.id,
-                }))
+            for scheduler in self.event_mail_template_id.scheduler_template_ids:
+                vals.append(
+                    (
+                        0,
+                        0,
+                        {
+                            "interval_nbr": scheduler.interval_nbr,
+                            "interval_unit": scheduler.interval_unit,
+                            "interval_type": scheduler.interval_type,
+                            "template_id": scheduler.template_id.id,
+                        },
+                    )
+                )
             self.event_mail_ids = vals
 
-    @api.onchange('event_type_id')
+    @api.onchange("event_type_id")
     def _onchange_type(self):
         """If a template is already set, we'll override the event.type
            schedulers"""
--- event_mail/models/event_type.py
+++ event_mail/models/event_type.py
@@ -4,20 +4,23 @@ from odoo import api, models
 
 
 class EventType(models.Model):
-    _inherit = 'event.type'
+    _inherit = "event.type"
 
     @api.model
     def _get_default_event_type_mail_ids(self):
-        if self.env.context.get('by_pass_config_template', False):
+        if self.env.context.get("by_pass_config_template", False):
             return super()._get_default_event_type_mail_ids()
-        company = self.env['res.company']._company_default_get('event.event')
+        company = self.env["res.company"]._company_default_get("event.event")
         event_mail_template_id = company.event_mail_template_id
         if event_mail_template_id:
-            return [{
-                'template_id': line.template_id,
-                'interval_nbr': line.interval_nbr,
-                'interval_unit': line.interval_unit,
-                'interval_type': line.interval_type}
-                for line in event_mail_template_id.scheduler_template_ids]
+            return [
+                {
+                    "template_id": line.template_id,
+                    "interval_nbr": line.interval_nbr,
+                    "interval_unit": line.interval_unit,
+                    "interval_type": line.interval_type,
+                }
+                for line in event_mail_template_id.scheduler_template_ids
+            ]
         else:
             return []
