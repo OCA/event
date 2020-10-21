847c7d2575ca6e52ec0707a4437b9daddd557df4
Ioan Galan <ioan@studio73.es>
2020-10-21 13:34:13 +0200

    [MIG] website_event_questions_by_ticket: Migration to 13.0

--- website_event_questions_by_ticket/__manifest__.py
+++ website_event_questions_by_ticket/__manifest__.py
@@ -4,12 +4,12 @@
 {
     "name": "Conditional Events Questions",
     "summary": "Events Questions conditional to the chosen ticket",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/event",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
-    "depends": ["website_event_sale", "website_event_questions",],
-    "data": ["views/event_event_views.xml", "views/event_templates.xml",],
+    "depends": ["website_event_sale", "website_event_questions"],
+    "data": ["views/event_event_views.xml", "views/event_templates.xml"],
 }
