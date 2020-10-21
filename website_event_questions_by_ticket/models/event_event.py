3510d3a8bcea01fa3031fd037cf7114d80a575e4
Ioan Galan <ioan@studio73.es>
2020-10-21 13:28:31 +0200

    [IMP] website_event_questions_by_ticket: black, isort, prettier

--- website_event_questions_by_ticket/models/event_event.py
+++ website_event_questions_by_ticket/models/event_event.py
@@ -5,25 +5,28 @@ from odoo import fields, models
 
 
 class EventEvent(models.Model):
-    _inherit = 'event.event'
+    _inherit = "event.event"
 
     def _get_specific_questions(self, ticket_id):
         self.ensure_one()
-        return self.specific_question_ids.filtered(lambda x: (
-            not x.restricted_ticket_ids or
-            ticket_id in x.restricted_ticket_ids.ids
-        ))
+        return self.specific_question_ids.filtered(
+            lambda x: (
+                not x.restricted_ticket_ids or ticket_id in x.restricted_ticket_ids.ids
+            )
+        )
 
     def _get_general_questions(self, ticket_ids):
         self.ensure_one()
-        return self.general_question_ids.filtered(lambda x: (
-            not x.restricted_ticket_ids or
-            (set(ticket_ids) & set(x.restricted_ticket_ids.ids))
-        ))
+        return self.general_question_ids.filtered(
+            lambda x: (
+                not x.restricted_ticket_ids
+                or (set(ticket_ids) & set(x.restricted_ticket_ids.ids))
+            )
+        )
 
 
 class EventQuestion(models.Model):
-    _inherit = 'event.question'
+    _inherit = "event.question"
 
     restricted_ticket_ids = fields.Many2many(
         comodel_name="event.event.ticket",
