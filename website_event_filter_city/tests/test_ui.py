956c005bd415508bf14c73493d1c59ee2bef5236
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-26 10:46:38 +0100

    [MIG] website_event_filter_selector: Migration to 13.0

--- website_event_filter_city/tests/test_ui.py
+++ website_event_filter_city/tests/test_ui.py
@@ -1,16 +1,11 @@
-# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2016 Tecnativa - Jairo Llopis
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo.tests.common import HttpCase
+from odoo.tests.common import HttpCase, tagged
 
 
+@tagged("post_install", "-at_install")
 class UICase(HttpCase):
     def test_ui_website(self):
         """Test frontend tour."""
-        self.browser_js(
-            url_path="/event",
-            code="odoo.__DEBUG__.services['web_tour.tour']"
-            ".run('website_event_filter_selector')",
-            ready="odoo.__DEBUG__.services['web_tour.tour']"
-            ".tours.website_event_filter_selector.ready",
-        )
+        self.start_tour("/event", "website_event_filter_city", login="portal")
