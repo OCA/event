c80fa80765180730ba8bd8a44308c40ac34b5595
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-26 08:40:08 +0100

    [IMP] website_event_filter_selector: black, isort, prettier

--- website_event_filter_city/static/src/js/website_event_filter_selector.tour.js
+++ website_event_filter_city/static/src/js/website_event_filter_selector.tour.js
@@ -1,25 +1,25 @@
 /* Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
  * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl). */
-odoo.define("website_event_filter_selector.tour", function (require) {
+odoo.define("website_event_filter_selector.tour", function(require) {
     "use strict";
     var base = require("web_editor.base");
-    var tour = require('web_tour.tour');
+    var tour = require("web_tour.tour");
     var filter = require("website_event_filter_selector");
 
     var id = filter.prototype.selector,
         $filters = $(id);
 
     // Get an element in the selection filters form
-    function sel (name) {
+    function sel(name) {
         return _.str.sprintf("%s select[name='%s']", id, name);
     }
 
     // Get an option value by its text
-    function opt_val (option_text) {
-        return function (action_helper) {
-            var option_id = this.$anchor.children(_.str.sprintf(
-                "option:contains('%s')", option_text
-            )).val();
+    function opt_val(option_text) {
+        return function(action_helper) {
+            var option_id = this.$anchor
+                .children(_.str.sprintf("option:contains('%s')", option_text))
+                .val();
             action_helper.text(option_id);
         };
     }
