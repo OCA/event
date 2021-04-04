57a781489e7ce60a2a04283e37b0ae8e4fc20599
David Alonso <david.alonso@solvos.es>
2021-04-04 12:37:11 +0200

    [MIG] event_contact: Migration to 13.0

--- event_contact/__manifest__.py
+++ event_contact/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Event Contacts",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "summary": "Add contacts to event and event type",
     "author": "OpenSynergy Indonesia, "
     "Tecnativa, "
