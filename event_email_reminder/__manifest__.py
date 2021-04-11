0cf3e3a5919b52bc1ec7dec602a2fbb6ad12b69f
David Alonso <david.alonso@solvos.es>
2021-04-11 11:42:46 +0200

    [IMP] event_email_reminder: black, isort, prettier

--- event_email_reminder/__manifest__.py
+++ event_email_reminder/__manifest__.py
@@ -8,16 +8,11 @@
     "summary": "Send an email before an event start",
     "version": "12.0.1.0.0",
     "category": "Event Management",
-    'website': 'http://www.tecnativa.com',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
+    "website": "http://www.tecnativa.com",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "event",
-    ],
-    "data": [
-        "data/event_email_reminder_data.xml",
-    ],
+    "depends": ["event",],
+    "data": ["data/event_email_reminder_data.xml",],
 }
