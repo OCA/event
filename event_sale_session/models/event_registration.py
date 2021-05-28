3e78fd14dee2793d466be9fc9d95f6ee8fad48bf
Carlos Roca <carlos.roca@tecnativa.com>
2021-05-28 13:06:17 +0200

    [IMP] event_sale_session: black, isort

--- event_sale_session/models/event_registration.py
+++ event_sale_session/models/event_registration.py
@@ -4,12 +4,11 @@ from odoo import api, models
 
 
 class EventRegistration(models.Model):
-    _inherit = 'event.registration'
+    _inherit = "event.registration"
 
     @api.model
     def _prepare_attendee_values(self, registration):
-        data = super(EventRegistration, self)._prepare_attendee_values(
-            registration)
-        session_id = registration['sale_order_line_id'].session_id.id
-        data.update({'session_id': session_id})
+        data = super(EventRegistration, self)._prepare_attendee_values(registration)
+        session_id = registration["sale_order_line_id"].session_id.id
+        data.update({"session_id": session_id})
         return data
