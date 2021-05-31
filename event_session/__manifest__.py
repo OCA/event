726f92ec5681214e7404ead09571d62d30f41421
Carlos Roca <carlos.roca@tecnativa.com>
2021-05-31 09:10:24 +0200

    [FIX] event_session: Error when seats_max is 0
    When we create a session with seats_max = 0, we get an error when we try
    to view it.
    
    Doing this changes we achieve to avoid None values on the fields
    seats_available and seats_available_pc

--- event_session/__manifest__.py
+++ event_session/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Event Sessions",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/oca/event.git",
