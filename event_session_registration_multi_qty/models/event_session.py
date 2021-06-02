fbec0d775bd1638acf076cf3e714b7e70dc80095
Carlos Roca <carlos.roca@tecnativa.com>
2021-06-02 09:06:00 +0200

    [IMP] event_session_registration_multi_qty: black, isort

--- event_session_registration_multi_qty/models/event_session.py
+++ event_session_registration_multi_qty/models/event_session.py
@@ -4,39 +4,44 @@ from odoo import api, models
 
 
 class EventSession(models.Model):
-    _inherit = 'event.session'
+    _inherit = "event.session"
 
     @api.multi
-    @api.depends('seats_max', 'registration_ids.state',
-                 'registration_ids.qty')
+    @api.depends("seats_max", "registration_ids.state", "registration_ids.qty")
     def _compute_seats(self):
         for session in self:
             if not session.event_id.registration_multi_qty:
                 return super(EventSession, self)._compute_seats()
             vals = {
-                'seats_unconfirmed': 0,
-                'seats_reserved': 0,
-                'seats_used': 0,
-                'seats_available': 0,
-                'seats_available_expected': 0,
+                "seats_unconfirmed": 0,
+                "seats_reserved": 0,
+                "seats_used": 0,
+                "seats_available": 0,
+                "seats_available_expected": 0,
             }
-            registrations = self.env['event.registration'].read_group([
-                ('session_id', '=', session.id),
-                ('state', 'in', ['draft', 'open', 'done'])
-            ], ['state', 'qty'], ['state'])
+            registrations = self.env["event.registration"].read_group(
+                [
+                    ("session_id", "=", session.id),
+                    ("state", "in", ["draft", "open", "done"]),
+                ],
+                ["state", "qty"],
+                ["state"],
+            )
             for registration in registrations:
-                if registration['state'] == 'draft':
-                    vals['seats_unconfirmed'] += registration['qty']
-                elif registration['state'] == 'open':
-                    vals['seats_reserved'] += registration['qty']
-                elif registration['state'] == 'done':
-                    vals['seats_used'] += registration['qty']
-            vals['seats_expected'] = (
-                vals['seats_unconfirmed'] + vals['seats_reserved'] +
-                vals['seats_used'])
+                if registration["state"] == "draft":
+                    vals["seats_unconfirmed"] += registration["qty"]
+                elif registration["state"] == "open":
+                    vals["seats_reserved"] += registration["qty"]
+                elif registration["state"] == "done":
+                    vals["seats_used"] += registration["qty"]
+            vals["seats_expected"] = (
+                vals["seats_unconfirmed"] + vals["seats_reserved"] + vals["seats_used"]
+            )
             if session.seats_max > 0:
-                vals['seats_available'] = session.seats_max - (
-                    vals['seats_reserved'] + vals['seats_used'])
-                vals['seats_available_expected'] = (
-                    session.seats_max - vals['seats_expected'])
+                vals["seats_available"] = session.seats_max - (
+                    vals["seats_reserved"] + vals["seats_used"]
+                )
+                vals["seats_available_expected"] = (
+                    session.seats_max - vals["seats_expected"]
+                )
             session.update(vals)
