5723ad391f1d0d72cc66f51f50dfb30d9ca8dc3b
Carlos Roca <carlos.roca@tecnativa.com>
2021-06-02 10:39:08 +0200

    [IMP] event_sale_registration_multi_qty: black, isort

--- event_sale_registration_multi_qty/__manifest__.py
+++ event_sale_registration_multi_qty/__manifest__.py
@@ -2,21 +2,15 @@
 # Copyright 2017 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Event Sale Registration Multi Qty',
-    'version': '12.0.1.0.0',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
+    "name": "Event Sale Registration Multi Qty",
+    "version": "12.0.1.0.0",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    'website': 'https://www.github.com/OCA/event',
-    'category': 'Marketing',
-    'summary': 'Allows sell registrations with more than one attendee',
-    'depends': [
-        'event_sale',
-        'event_registration_multi_qty',
-    ],
-    'data': [
-        'wizards/event_edit_registration.xml',
-    ],
-    'installable': True,
-    'auto_install': True,
+    "website": "https://www.github.com/OCA/event",
+    "category": "Marketing",
+    "summary": "Allows sell registrations with more than one attendee",
+    "depends": ["event_sale", "event_registration_multi_qty",],
+    "data": ["wizards/event_edit_registration.xml",],
+    "installable": True,
+    "auto_install": True,
 }
--- event_sale_registration_multi_qty/models/sale_order.py
+++ event_sale_registration_multi_qty/models/sale_order.py
@@ -7,29 +7,34 @@ from odoo import api, models
 
 class SaleOrderLine(models.Model):
 
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     @api.multi
-    def _update_registrations(self, confirm=True, cancel_to_draft=False,
-                              registration_data=None):
+    def _update_registrations(
+        self, confirm=True, cancel_to_draft=False, registration_data=None
+    ):
         """Update registrations on events with multi qty enabled"""
-        Registration = self.env['event.registration']
-        for so_line in self.filtered('event_id'):
+        Registration = self.env["event.registration"]
+        for so_line in self.filtered("event_id"):
             if not so_line.event_id.registration_multi_qty:
                 super(SaleOrderLine, so_line)._update_registrations(
-                    confirm=confirm, cancel_to_draft=cancel_to_draft,
-                    registration_data=registration_data)
+                    confirm=confirm,
+                    cancel_to_draft=cancel_to_draft,
+                    registration_data=registration_data,
+                )
                 continue
             product_uom_qty = so_line.product_uom_qty
             # Set temporarily the order line to one avoiding to create more
             # than one registration
             so_line.product_uom_qty = 1
             super(SaleOrderLine, so_line)._update_registrations(
-                confirm=confirm, cancel_to_draft=cancel_to_draft,
-                registration_data=registration_data)
+                confirm=confirm,
+                cancel_to_draft=cancel_to_draft,
+                registration_data=registration_data,
+            )
             # Set the so line qty back and set the registration qty
             so_line.product_uom_qty = product_uom_qty
-            registration = Registration.search([
-                ('sale_order_line_id', '=', so_line.id)
-            ])
+            registration = Registration.search(
+                [("sale_order_line_id", "=", so_line.id)]
+            )
             registration.qty = int(product_uom_qty)
