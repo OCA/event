9f1ca3d9864c148e04d3c26dfdfeaefb220a99ee
Jairo Llopis <jairo.llopis@tecnativa.com>
2021-08-13 09:20:20 +0100

    [FIX] website_event_questions_free_text: integration with website_event_sale
    When this module was tested on a database that had `website_event_sale` installed, tests failed with:
    
    ```
    2021-08-13 07:36:36,365 23030 INFO openerp_test odoo.addons.website_event_questions_free_text.tests.test_free_text: Starting TestFreeText.test_free_text_answer ...
    
    2021-08-13 07:36:36,491 23030 INFO openerp_test odoo.addons.base.models.res_users: Login successful for db:openerp_test login:demo from 127.0.0.1
    
    2021-08-13 07:36:36,541 23030 INFO ? odoo.http: HTTP Configuring static files
    
    2021-08-13 07:36:36,547 23030 INFO openerp_test odoo.addons.base.models.ir_http: Generating routing map for key 1
    
    2021-08-13 07:36:36,832 23030 WARNING openerp_test odoo.addons.http_routing.models.ir_http: 400 Bad Request:
    
    Traceback (most recent call last):
    
    File "/home/travis/odoo-13.0/odoo/addons/base/models/ir_http.py", line 234, in _dispatch
    
    result = request.dispatch()
    
    File "/home/travis/odoo-13.0/odoo/http.py", line 809, in dispatch
    
    r = self._call_function(**self.params)
    
    File "/home/travis/odoo-13.0/odoo/http.py", line 350, in _call_function
    
    return checked_call(self.db, *args, **kwargs)
    
    File "/home/travis/odoo-13.0/odoo/service/model.py", line 94, in wrapper
    
    return f(dbname, *args, **kwargs)
    
    File "/home/travis/odoo-13.0/odoo/http.py", line 339, in checked_call
    
    result = self.endpoint(*a, **kw)
    
    File "/home/travis/odoo-13.0/odoo/http.py", line 915, in __call__
    
    return self.method(*args, **kw)
    
    File "/home/travis/odoo-13.0/odoo/http.py", line 515, in response_wrap
    
    response = f(*args, **kw)
    
    File "/home/travis/odoo-13.0/addons/website_event_sale/controllers/main.py", line 40, in registration_confirm
    
    cart_values = order.with_context(event_ticket_id=ticket.id, fixed_price=True)._cart_update(product_id=ticket.product_id.id, add_qty=1, registration_data=[registration])
    
    File "/home/travis/odoo-13.0/addons/website_event_sale/models/sale_order.py", line 93, in _cart_update
    
    values.update(super(SaleOrder, self)._cart_update(product_id, line_id, add_qty, set_qty, **kwargs))
    
    File "/home/travis/odoo-13.0/addons/website_sale/models/sale_order.py", line 171, in _cart_update
    
    raise UserError(_("The given product does not exist therefore it cannot be added to cart."))
    
    odoo.exceptions.UserError: ('The given product does not exist therefore it cannot be added to cart.', '')
    ```
    
    With this patch, I'm fixing that problem and besides fixing the tests logic. As these tests were using `MockRequest`, there was no need for them to run as an `HttpCase`, and that in fact was raising the integration problem because those requests ran only in `post_install` mode.
    
    So, tests are now more resilient and fast.
    
    @Tecnativa TT31311

--- website_event_questions_free_text/tests/test_free_text.py
+++ website_event_questions_free_text/tests/test_free_text.py
@@ -3,66 +3,63 @@
 
 from dateutil.relativedelta import relativedelta
 
-from odoo import fields, http
-from odoo.tests import HttpCase
+from odoo import fields
+from odoo.tests import SavepointCase
+from odoo.tools.misc import mute_logger
 
 from odoo.addons.website.tools import MockRequest
 
+from ..controllers.main import WebsiteEvent
 
-class TestFreeText(HttpCase):
-    def setUp(self):
-        super().setUp()
+
+class TestFreeText(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         now = fields.Datetime.now()
-        self.website = self.env["website"].browse(1)
-        self.event_question_free_text = "Free Text"
-        self.event_1 = self.env["event.event"].create(
+        cls.user_demo = cls.env.ref("base.user_demo")
+        cls.website_event_controller = WebsiteEvent()
+        cls.website = cls.env["website"].browse(1)
+        cls.event_question_free_text = "Free Text"
+        cls.event_1 = cls.env["event.event"].create(
             {
                 "name": "Event One",
-                "user_id": self.env.ref("base.user_admin").id,
+                "user_id": cls.env.ref("base.user_admin").id,
                 "date_begin": now + relativedelta(days=1),
                 "date_end": now + relativedelta(days=3),
-                "organizer_id": self.env.ref("base.res_partner_1").id,
-                "event_type_id": self.env.ref("event.event_type_1").id,
+                "organizer_id": cls.env.ref("base.res_partner_1").id,
+                "event_type_id": cls.env.ref("event.event_type_1").id,
                 "website_published": True,
                 "description": "Test",
                 "auto_confirm": True,
-                "website_id": self.website.id,
+                "website_id": cls.website.id,
                 "question_ids": [(0, 0, {"title": "Question One", "free_text": True})],
             }
         )
 
     def test_free_text_answer(self):
-        with MockRequest(self.env, website=self.website):
-            self.confirm(self.event_1)
-            self.assertEqual(
-                self.event_1.registration_ids.free_answer_ids.answer,
-                self.event_question_free_text,
-            )
-        return True
+        self.confirm(self.event_1)
+        self.assertEqual(
+            self.event_1.registration_ids.free_answer_ids.answer,
+            self.event_question_free_text,
+        )
 
     def test_free_text_display_name(self):
-        with MockRequest(self.env, website=self.website):
-            self.confirm(self.event_1)
-            display_name = "{}: {}".format(
-                self.event_1.question_ids.title, self.event_question_free_text
-            )
-            self.assertEqual(
-                self.event_1.registration_ids.free_answer_ids.display_name, display_name
-            )
-        return True
-
-    def confirm(self, event):
-        self.authenticate("demo", "demo")
-        self.url_open(
-            "/event/{}/registration/confirm".format(event.id),
-            data=self._build_confirmation_req_values(event),
+        self.confirm(self.event_1)
+        display_name = "{}: {}".format(
+            self.event_1.question_ids.title, self.event_question_free_text
         )
-        return True
+        self.assertEqual(
+            self.event_1.registration_ids.free_answer_ids.display_name, display_name
+        )
 
-    def _build_confirmation_req_values(self, event):
-        values = self._prepare_registration_confirm_values(event)
-        values["csrf_token"] = http.WebRequest.csrf_token(self)
-        return values
+    # HACK https://github.com/odoo/odoo/issues/75061
+    @mute_logger("odoo.http")
+    def confirm(self, event):
+        with MockRequest(self.env(user=self.user_demo), website=self.website):
+            self.website_event_controller.registration_confirm(
+                event, **self._prepare_registration_confirm_values(event)
+            )
 
     def _prepare_registration_confirm_values(self, event, counter=1, ticket_id=0):
         return {
