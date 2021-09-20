4eab3d6e551f363c96a5684e9dd892d4faa77fa8
mle <marie.lejeune@acsone.eu>
2021-09-20 14:47:06 +0200

    [IMP] website_event_questions_template: black, isort, prettier

--- website_event_questions_template/tests/test_website_event_questions_template.py
+++ website_event_questions_template/tests/test_website_event_questions_template.py
@@ -1,77 +1,95 @@
 # Copyright 2017 Tecnativa - Sergio Teruel
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo.tests.common import SavepointCase
 from odoo.exceptions import UserError
+from odoo.tests.common import SavepointCase
 
 
 class EventQuestionTemplate(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super(EventQuestionTemplate, cls).setUpClass()
-        cls.template1 = cls.env['event.question.template'].create({
-            'name': 'Template test 01',
-        })
-        cls.template2 = cls.env['event.question.template'].create({
-            'name': 'Template test 01',
-            'question_ids': [(0, 0, {
-                'title': 'question T2 01',
-                'is_individual': True,
-                'answer_ids': [
-                    (0, 0, {'name': 'Answer 01 test 01'}),
-                    (0, 0, {'name': 'Answer 02 test 01'}),
-                ]
-            })]
-        })
+        cls.template1 = cls.env["event.question.template"].create(
+            {
+                "name": "Template test 01",
+            }
+        )
+        cls.template2 = cls.env["event.question.template"].create(
+            {
+                "name": "Template test 01",
+                "question_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "title": "question T2 01",
+                            "is_individual": True,
+                            "answer_ids": [
+                                (0, 0, {"name": "Answer 01 test 01"}),
+                                (0, 0, {"name": "Answer 02 test 01"}),
+                            ],
+                        },
+                    )
+                ],
+            }
+        )
 
     def test_event_template(self):
         # Create an event
         vals = {
-            'name': 'Event test',
-            'date_begin': '2017-05-01',
-            'date_end': '2017-06-01',
-            'auto_confirm': False,
-            'event_question_template_id': self.template1.id,
+            "name": "Event test",
+            "date_begin": "2017-05-01",
+            "date_end": "2017-06-01",
+            "auto_confirm": False,
+            "event_question_template_id": self.template1.id,
         }
-        event = self.env['event.event'].create(vals)
+        event = self.env["event.event"].create(vals)
         event.load_question_template()
         self.assertFalse(
             event.question_ids,
-            'Event Question Template: Questions created for this event')
+            "Event Question Template: Questions created for this event",
+        )
 
         # Change template in event
         event.event_question_template_id = self.template2
         event.load_question_template()
         self.assertEqual(
-            len(event.question_ids), 1,
-            'Event Question Template: Questions only one')
+            len(event.question_ids), 1, "Event Question Template: Questions only one"
+        )
 
         self.assertEqual(
-            len(event.question_ids.answer_ids), 2,
-            'Event Question Template: Answer two')
+            len(event.question_ids.answer_ids), 2, "Event Question Template: Answer two"
+        )
 
     def test_event_template_not_allowed(self):
         """Create an event with two questions, if I load the template raise
-           error
+        error
         """
         vals = {
-            'name': 'Event test',
-            'date_begin': '2017-05-01',
-            'date_end': '2017-06-01',
-            'auto_confirm': False,
-            'event_question_template_id': self.template2.id,
-            'question_ids': [
-                (0, 0, {
-                    'title': 'question T2 01',
-                    'is_individual': True,
-                }),
-                (0, 0, {
-                    'title': 'question T2 02',
-                    'is_individual': True,
-                }),
+            "name": "Event test",
+            "date_begin": "2017-05-01",
+            "date_end": "2017-06-01",
+            "auto_confirm": False,
+            "event_question_template_id": self.template2.id,
+            "question_ids": [
+                (
+                    0,
+                    0,
+                    {
+                        "title": "question T2 01",
+                        "is_individual": True,
+                    },
+                ),
+                (
+                    0,
+                    0,
+                    {
+                        "title": "question T2 02",
+                        "is_individual": True,
+                    },
+                ),
             ],
         }
-        event = self.env['event.event'].create(vals)
+        event = self.env["event.event"].create(vals)
         with self.assertRaises(UserError):
             event.load_question_template()
