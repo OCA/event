35422a5e02251b514723cdd7ea0c56377b306653
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-11-09 08:59:18 -0500

    [IMP] website_event_questions_template: black, isort, prettier

--- website_event_questions_template/__manifest__.py
+++ website_event_questions_template/__manifest__.py
@@ -2,19 +2,18 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Website Event Questions Template',
-    'summary': 'Set question templates for events',
-    'version': '12.0.1.0.0',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
+    "name": "Website Event Questions Template",
+    "summary": "Set question templates for events",
+    "version": "12.0.1.0.0",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
-    'website': 'https://github.com/OCA/event',
-    'category': 'Marketing',
-    'depends': ['website_event_questions'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/event_view.xml',
-        'views/website_event_questions_template_view.xml',
+    "website": "https://github.com/OCA/event",
+    "category": "Marketing",
+    "depends": ["website_event_questions"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/event_view.xml",
+        "views/website_event_questions_template_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
