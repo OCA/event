865c1440a1869ecd13b71fc8db51428ac95db4dd
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-12-09 22:55:29 -0500

    [IMP] event_registration_mass_mailing: black, isort, prettier

--- event_registration_mass_mailing/__manifest__.py
+++ event_registration_mass_mailing/__manifest__.py
@@ -3,21 +3,17 @@
 # Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "Put event registrations emails into mailing lists",
-    'category': 'Marketing',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'event',
-        'mass_mailing'
+    "name": "Put event registrations emails into mailing lists",
+    "category": "Marketing",
+    "version": "12.0.1.0.0",
+    "depends": ["event", "mass_mailing"],
+    "data": [
+        "views/event_registration.xml",
+        "wizard/event_registration_mail_list_wizard.xml",
     ],
-    'data': [
-        'views/event_registration.xml',
-        'wizard/event_registration_mail_list_wizard.xml',
-    ],
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'http://github.com/OCA/event',
-    'license': 'AGPL-3',
-    'installable': True,
-    'auto_install': False,
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/event",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": False,
 }
