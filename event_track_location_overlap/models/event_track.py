0f17aa110308b4958f49d80bb46949e514f00b2c
Víctor Martínez <victor.martinez@tecnativa.com>
2022-01-24 10:50:18 +0100

    [MIG] event_track_location_overlap: Migration to 13.0 TT27583
    [UPD] Update event_track_location_overlap.pot
    
    [UPD] README.rst

--- event_track_location_overlap/models/event_track.py
+++ event_track_location_overlap/models/event_track.py
@@ -7,8 +7,8 @@ from odoo import api, models
 class EventTrack(models.Model):
     _inherit = "event.track"
 
-    @api.multi
     @api.constrains("date", "duration", "location_id", "stage_id")
     def _check_location_overlap(self):
         """Make sure no location overlaps happen."""
-        self.mapped("location_id")._check_overlappable()
+        for location in self.mapped("location_id"):
+            location._check_overlappable_one()
