b016976d525b1fc3183e4eb0f6fb0e25625cd9d6
Víctor Martínez <victor.martinez@tecnativa.com>
2022-01-25 13:04:43 +0100

    [IMP] event_sale_reservation: black, isort, prettier

--- event_sale_reservation/models/event_type.py
+++ event_sale_reservation/models/event_type.py
@@ -50,12 +50,7 @@ class EventType(models.Model):
 
     def action_open_sale_orders(self):
         """Display SO that include reservations."""
-        sol = self.env["sale.order.line"].search(
-            self._seats_reservation_domain(),
-        )
-        result = self.env["ir.actions.act_window"].for_xml_id(
-            "sale",
-            "action_orders",
-        )
+        sol = self.env["sale.order.line"].search(self._seats_reservation_domain(),)
+        result = self.env["ir.actions.act_window"].for_xml_id("sale", "action_orders",)
         result["domain"] = [("order_line", "in", sol.ids)]
         return result
