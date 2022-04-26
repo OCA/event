357ca84730b68b4bbf7e4cab768ea085070c7b75
Luis D. Lafaurie <luis.lafaurie@tecnativa.com>
2022-04-26 11:11:35 +0200

    [MIG] event_registration_partner_unique: Migration to 14.0

--- event_registration_partner_unique/tests/test_event.py
+++ event_registration_partner_unique/tests/test_event.py
@@ -2,9 +2,8 @@
 # Copyright 2020 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
-
-from .. import exceptions
 
 
 class DuplicatedPartnerCase(TransactionCase):
@@ -28,7 +27,7 @@ class DuplicatedPartnerCase(TransactionCase):
     def test_forbidden(self):
         """Cannot when it is forbidden."""
         self.event.forbid_duplicates = True
-        with self.assertRaises(exceptions.DuplicatedPartnerError):
+        with self.assertRaises(ValidationError):
             self.registration.copy(
                 {"attendee_partner_id": self.registration.attendee_partner_id.id}
             )
@@ -36,17 +35,17 @@ class DuplicatedPartnerCase(TransactionCase):
     def test_saved_in_exception(self):
         """The failing partners are saved in the exception."""
         self.event.forbid_duplicates = True
-        try:
+        with self.assertRaisesRegex(
+            ValidationError, "Duplicated partners found in event"
+        ):
             self.registration.copy(
                 {"attendee_partner_id": self.registration.attendee_partner_id.id}
             )
-        except exceptions.DuplicatedPartnerError as error:
-            self.assertEqual(error._kwargs["registrations"], self.registration)
 
     def test_duplicates_already_exist(self):
         """Cannot forbid what already happened."""
         self.registration.copy(
             {"attendee_partner_id": self.registration.attendee_partner_id.id}
         )
-        with self.assertRaises(exceptions.DuplicatedPartnerError):
+        with self.assertRaises(ValidationError):
             self.event.forbid_duplicates = True
