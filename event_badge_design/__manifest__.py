144db5b0c90da83a07ffd9d0e2c8b160814d51a0
Holger Brunn <mail@hunki-enterprises.com>
2022-10-03 13:47:06 +0200

    [ADD] event_badge_design

---
+++ event_badge_design/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Event badge design",
+    "summary": "Allows you to customize event badges",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Marketing/Events",
+    "website": "https://github.com/OCA/event",
+    "author": "Hunki Enterprises BV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "event",
+    ],
+    "data": [
+        "views/event_event.xml",
+    ],
+}
---
+++ event_badge_design/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module allows you to customize event badges. You'll find tabs on an event to edit the different parts of the badge.
