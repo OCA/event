c2822cc87fb1cf2ba65093550e92fe9dd230ee63
Stefan <stefan.ungureanu@tecnativa.com>
2023-01-23 10:21:21 +0100

    [MIG] crm_event: Migration to 15.0

--- crm_event/__manifest__.py
+++ crm_event/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "CRM Event Category",
     "summary": "Link opportunities to event categories",
-    "version": "13.0.1.1.0",
+    "version": "15.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Event Management",
     "website": "https://github.com/OCA/event",
