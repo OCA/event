597c532dc72169de5a152e9bb0fd75e771993d84
Camille Morand <camille.morand@camptocamp.com>
2023-03-20 10:17:41 +0100

    [MIG] event_sale_reserve_registration: Migration to 15.0

--- event_sale_reserve_registration/__manifest__.py
+++ event_sale_reserve_registration/__manifest__.py
@@ -1,12 +1,12 @@
 {
     "name": "Events Sale Reserve Registrations",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Marketing/Events",
     "website": "https://github.com/OCA/event",
     "summary": "Allow to reserve registrations before confirming sale order",
-    "depends": ["event_sale_attendees"],
+    "depends": ["event_sale"],
     "data": ["views/sale_view.xml"],
     "installable": True,
 }
