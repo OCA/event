b2eb8095a5e6fe68af6c48098640d83314f76a4d
David <david.vidal@tecnativa.com>
2023-04-03 13:45:06 +0200

    [MIG] website_event_filter_city: Migration to 15.0
    TT41028

--- website_event_filter_city/__manifest__.py
+++ website_event_filter_city/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Website Event Filter City",
     "summary": "Add a customizable top area to filter events with city",
-    "version": "13.0.1.0.2",
+    "version": "15.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/event",
     "author": "Antiun Ingeniería S.L., "
@@ -13,9 +13,13 @@
     "Odoo Community Association (OCA)",
     "maintainers": ["Yajo"],
     "license": "LGPL-3",
-    "application": False,
     "installable": True,
     "depends": ["website_event"],
     "data": ["templates/event.xml"],
-    "demo": ["demo/assets.xml", "demo/tour_data.xml"],
+    "demo": ["demo/tour_data.xml"],
+    "assets": {
+        "web.assets_tests": [
+            "/website_event_filter_city/static/src/js/*.js",
+        ]
+    },
 }
