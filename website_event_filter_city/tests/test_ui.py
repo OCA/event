1ee0d9f0bd05b98632706f26d35407df3594fe4c
David <david.vidal@tecnativa.com>
2023-04-03 13:45:06 +0200

    [MIG] website_event_filter_city: Migration to 15.0
    TT41028

--- website_event_filter_city/tests/test_ui.py
+++ website_event_filter_city/tests/test_ui.py
@@ -6,6 +6,16 @@ from odoo.tests.common import HttpCase, tagged
 
 @tagged("post_install", "-at_install")
 class UICase(HttpCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        websites = cls.env["website"].search([])
+        # Activate country filters in all websites
+        for website in websites:
+            cls.env.ref("website_event.event_location").with_context(
+                website_id=website.id
+            ).active = True
+
     def test_ui_website(self):
         """Test frontend tour."""
         self.start_tour("/event", "website_event_filter_city", login="portal")
