1ddbae2352f3a3b3248e4e17387b5982ffe13c80
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2023-05-10 15:48:38 +0200

    [MIG] event_mail: Migration to 15.0

--- event_mail/models/event_type.py
+++ event_mail/models/event_type.py
@@ -1,33 +1,21 @@
 # Copyright 2019 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models
+from odoo import Command, models
 
 
 class EventType(models.Model):
     _inherit = "event.type"
 
-    def _compute_event_type_mail_ids(self):
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
         event_mail_template_id = self.env.company.event_mail_template_id
-        for template in self:
-            if (
-                template.use_mail_schedule
-                and not template.event_type_mail_ids
-                and event_mail_template_id
-            ):
-                template.event_type_mail_ids = [
-                    (
-                        0,
-                        0,
-                        {
-                            attribute_name: line[attribute_name]
-                            if not isinstance(line[attribute_name], models.BaseModel)
-                            else line[attribute_name].id
-                            for attribute_name in self.env[
-                                "event.type.mail"
-                            ]._get_event_mail_fields_whitelist()
-                        },
-                    )
-                    for line in event_mail_template_id.scheduler_template_ids
-                ]
-            else:
-                template.event_type_mail_ids = [(5, 0)]
+        if event_mail_template_id:
+            res.update(
+                {
+                    "event_type_mail_ids": [
+                        Command.create(line._prepare_event_mail_values())
+                        for line in event_mail_template_id.scheduler_template_ids
+                    ]
+                }
+            )
+        return res
