970fdb377fb40dc86c5aa23d60e7bbc0d3af200b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-08-03 19:42:56 +0200

    [ADD] event_min_seat: Minimum seats in events
    This module recovers the lost feature for specifying the minimum seats
    required for an event, and showing such events on red in the list view
    if the minimum seats are not satisfied.
    
    TT42014

---
+++ event_min_seat/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Minimum seats in events",
+    "version": "15.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/event",
+    "category": "Marketing",
+    "depends": ["event"],
+    "data": [
+        "views/event_event_views.xml",
+        "views/event_type_views.xml",
+    ],
+    "installable": True,
+}
