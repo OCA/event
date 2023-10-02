6a1bf1a159bef370c3024da12ee1344445aff8f9
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-10-02 09:27:33 -0300

    [MIG] website_event_questions_by_ticket: Migration to 16.0
    [MIG] website_event_questions_by_ticket: Migration to 16.0

--- website_event_questions_by_ticket/tests/test_ticket.py
+++ website_event_questions_by_ticket/tests/test_ticket.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Studio73 - Ioan Galan
+# Copyright 2023 Tecnativa - Carolina Fernandez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from dateutil.relativedelta import relativedelta
@@ -8,36 +9,37 @@ from odoo.tests import HttpCase
 
 
 class TestTicket(HttpCase):
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         now = fields.Datetime.now()
-        self.website = self.env["website"].browse(1)
-        self.event_question_free_text = "Free Text"
-        self.event_1 = self.env["event.event"].create(
+        cls.website = cls.env["website"].browse(1)
+        cls.event_question_free_text = "Free Text"
+        cls.event_1 = cls.env["event.event"].create(
             {
                 "name": "Event One",
-                "user_id": self.env.ref("base.user_admin").id,
+                "user_id": cls.env.ref("base.user_admin").id,
                 "date_begin": now + relativedelta(days=1),
                 "date_end": now + relativedelta(days=3),
-                "organizer_id": self.env.ref("base.res_partner_1").id,
-                "event_type_id": self.env.ref("event.event_type_1").id,
+                "organizer_id": cls.env.ref("base.res_partner_1").id,
+                "event_type_id": cls.env.ref("event.event_type_1").id,
                 "website_published": True,
                 "description": "Test",
                 "auto_confirm": True,
-                "website_id": self.website.id,
+                "website_id": cls.website.id,
             }
         )
-        self.ticket_1 = self.env["event.event.ticket"].create(
-            {"name": "Ticket One", "event_id": self.event_1.id}
+        cls.ticket_1 = cls.env["event.event.ticket"].create(
+            {"name": "Ticket One", "event_id": cls.event_1.id}
         )
-        self.ticket_2 = self.env["event.event.ticket"].create(
-            {"name": "Ticket Two", "event_id": self.event_1.id}
+        cls.ticket_2 = cls.env["event.event.ticket"].create(
+            {"name": "Ticket Two", "event_id": cls.event_1.id}
         )
-        self.question_1 = self.env["event.question"].create(
+        cls.question_1 = cls.env["event.question"].create(
             {
                 "title": "Question Two",
-                "event_id": self.event_1.id,
-                "restricted_ticket_ids": [(6, 0, self.ticket_1.ids)],
+                "event_id": cls.event_1.id,
+                "restricted_ticket_ids": [(6, 0, cls.ticket_1.ids)],
             }
         )
 
