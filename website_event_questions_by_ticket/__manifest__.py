b3c7152f34b3b7fe104c79a492e5cf67601b7240
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-10-02 09:27:33 -0300

    [MIG] website_event_questions_by_ticket: Migration to 16.0
    [MIG] website_event_questions_by_ticket: Migration to 16.0

--- website_event_questions_by_ticket/__manifest__.py
+++ website_event_questions_by_ticket/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright 2019 Tecnativa - Pedro M. Baeza
+# Copyright 2023 Tecnativa - Carolina Fernandez
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Conditional Events Questions",
     "summary": "Events Questions conditional to the chosen ticket",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/event",
     "author": "Tecnativa, Odoo Community Association (OCA)",
