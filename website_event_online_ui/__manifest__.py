d86dafccddad816dc8d72e03e2adb3ddb20c76d9
thibaud <thibaud@le-filament.com>
2024-07-17 11:29:41 +0200

    [ADD] website_event_online_ui module

---
+++ website_event_online_ui/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Le Filament (https://le-filament.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
+{
+    "name": "Improve website event views",
+    "version": "16.0.1.0.1",
+    "depends": ["event", "website_event"],
+    "data": [
+        "views/event_event_views.xml",
+        "views/event_templates_list.xml",
+        "views/event_templates_page_registration.xml",
+    ],
+    "author": "Le Filament, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/event",
+    "maintainers": [],
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
+}
