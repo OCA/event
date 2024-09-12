8834b47d3238f02f0a626a277ed4fefb9fc7e598
Rémy Taymans <remytms@tsmail.eu>
2024-09-12 15:18:49 +0200

    [ADD] e_track_equipment: constraint on equipment_line

--- event_track_equipment/models/event_track_equipment_line.py
+++ event_track_equipment/models/event_track_equipment_line.py
@@ -12,3 +12,12 @@ class EventTrackEquipmentLine(models.Model):
     track_id = fields.Many2one("event.track", string="Track")
     equipment_id = fields.Many2one("event.track.equipment", string="Equipment")
     quantity = fields.Integer(default=1)
+
+    _sql_constraints = [
+        (
+            "event_track_equipment_line_equipment_uniq",
+            "unique (track_id, equipment_id)",
+            "Duplicate equipments on a track is not allowed, modify "
+            "quantity of existing line.",
+        )
+    ]
