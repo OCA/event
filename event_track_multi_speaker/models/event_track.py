1344421ea11f2f7d3072c574524d730d2b1ed6e9
Rémy Taymans <remytms@tsmail.eu>
2024-09-18 22:54:06 +0200

    [FIX] e_t_multi_speaker: fix wrong copyright

--- event_track_multi_speaker/models/event_track.py
+++ event_track_multi_speaker/models/event_track.py
@@ -1,6 +1,6 @@
-# Copyright 2017 David Vidal<david.vidal@tecnativa.com>
-# Copyright 2017 Tecnativa - Pedro M. Baeza
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# SPDX-FileCopyrightText: 2024 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 from odoo import fields, models
 
