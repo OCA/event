761892b8bc44f80d3963c5c496e86902ac0a2b48
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-04-22 10:55:33 +0200

    [MIG] website_event_crm_invitation: Migration to version 17.0
    TT51965

--- website_event_crm_invitation/models/event_type.py
+++ website_event_crm_invitation/models/event_type.py
@@ -1,7 +1,9 @@
 # Copyright 2021 Tecnativa - Jairo Llopis
+# Copyright 2025 Tecnativa - Pilar Vargas
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+from odoo.osv import expression
 
 
 class EventType(models.Model):
@@ -9,12 +11,29 @@ class EventType(models.Model):
 
     def _published_events_domain(self):
         """Get domain for open and published events of this category."""
-        domain = self._events_domain()
-        domain += [
+        base_domain = self._events_domain()
+        base_domain += [
             ("date_begin", ">", fields.Datetime.now()),
             ("website_published", "=", True),
-            "|",
-            ("seats_limited", "=", False),
-            ("seats_available", ">", 0),
         ]
+        domain = base_domain + [
+            ("seats_limited", "=", False),
+        ]
+        # Search limited events with available seats (not storable)
+        events_with_seats = self.env["event.event"].search(
+            base_domain
+            + [
+                ("seats_limited", "=", True),
+            ]
+        )
+        valid_event_ids = events_with_seats.filtered(
+            lambda e: e.seats_available > 0
+        ).ids
+        if valid_event_ids:
+            domain = expression.OR(
+                [
+                    domain,
+                    [("id", "in", valid_event_ids)],
+                ]
+            )
         return domain
