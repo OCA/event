50c28c811d965dc05971347a2679c485037b15cb
Joel Estrada <joel@moduon.team>
2025-08-22 10:49:07 +0200

    [MIG] website_event_contact: Migration to 18.0

--- website_event_contact/__manifest__.py
+++ website_event_contact/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Website Event Contacts",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "summary": "Display your event contacts on your event page",
     "author": "Moduon,OpenSynergy Indonesia,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/event",
