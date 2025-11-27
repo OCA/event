acd10c2be9108ac6f8f2d11af327ac0256ea4a85
Maxime Franco <maxime.franco@acsone.eu>
2025-11-27 11:58:32 +0100

    [MIG] website_event_questions_by_ticket: Migration to 19.0
    TT63416
    
    Co-authored-by: Maxime Franco <maxime.franco@acsone.eu>

--- website_event_questions_by_ticket/tests/test_ticket.py
+++ website_event_questions_by_ticket/tests/test_ticket.py
@@ -4,28 +4,22 @@
 
 from dateutil.relativedelta import relativedelta
 
-from odoo import fields
-from odoo.tests import HttpCase
+from odoo import Command, fields
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestTicket(HttpCase):
+class TestTicket(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         now = fields.Datetime.now()
-        cls.website = cls.env["website"].browse(1)
-        cls.event_question_free_text = "Free Text"
         cls.event_1 = cls.env["event.event"].create(
             {
                 "name": "Event One",
-                "user_id": cls.env.ref("base.user_admin").id,
                 "date_begin": now + relativedelta(days=1),
                 "date_end": now + relativedelta(days=3),
-                "organizer_id": cls.env.ref("base.res_partner_1").id,
-                "event_type_id": cls.env.ref("event.event_type_1").id,
-                "website_published": True,
-                "description": "Test",
-                "website_id": cls.website.id,
+                "organizer_id": cls.partner.id,
             }
         )
         cls.ticket_1 = cls.env["event.event.ticket"].create(
@@ -37,17 +31,19 @@ class TestTicket(HttpCase):
         cls.question_1 = cls.env["event.question"].create(
             {
                 "title": "Question Two",
-                "event_id": cls.event_1.id,
-                "restricted_ticket_ids": [(6, 0, cls.ticket_1.ids)],
+                "event_ids": [Command.set(cls.event_1.ids)],
+                "restricted_ticket_ids": [Command.set(cls.ticket_1.ids)],
             }
         )
 
     def test_specific_questions(self):
-        specific_questions = self.event_1._get_specific_questions(self.ticket_1.id)
-        self.assertTrue(self.question_1 in specific_questions)
-        return True
+        specific_questions_1 = self.event_1._get_specific_questions(self.ticket_1.id)
+        self.assertIn(self.question_1, specific_questions_1)
+        specific_questions_2 = self.event_1._get_specific_questions(self.ticket_2.id)
+        self.assertNotIn(self.question_1, specific_questions_2)
 
     def test_general_questions(self):
-        general_questions = self.event_1._get_general_questions(self.ticket_2.ids)
-        self.assertFalse(self.question_1 in general_questions)
-        return True
+        general_questions_1 = self.event_1._get_general_questions(self.ticket_1.ids)
+        self.assertNotIn(self.question_1, general_questions_1)
+        general_questions_2 = self.event_1._get_general_questions(self.ticket_2.ids)
+        self.assertNotIn(self.question_1, general_questions_2)
