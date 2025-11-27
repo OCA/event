58d5584ca0755526b0100c6c668552e07bcb072c
Maxime Franco <maxime.franco@acsone.eu>
2025-11-27 13:09:52 +0100

    [IMP] website_event_require_legal: pre-commit auto fixes

--- website_event_require_legal/__manifest__.py
+++ website_event_require_legal/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Website Event Require Legal",
     "version": "18.0.1.0.0",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/event",
     "category": "Event",
     "depends": ["website_event"],
