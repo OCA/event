3b54cc507012cd27b5b08f6ade702c78e2daec49
Holger Brunn <mail@hunki-enterprises.com>
2026-03-25 06:52:55 +0100

    [MIG] website_event_external: Migration to 18.0

--- website_event_external/__manifest__.py
+++ website_event_external/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "External Event",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Marketing/Events",
     "author": "Le Filament, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/event",
