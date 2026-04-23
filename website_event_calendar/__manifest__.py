cbc97394e03dcf78b1d51dc6a8b1294d6bce5674
tarteo <d.sluijk@onestein.nl>
2026-04-23 11:51:03 +0200

    [ADD] website_event_calendar: Extra generic and other calendar options to event page

---
+++ website_event_calendar/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Events: generic & other calendar options",
+    "version": "18.0.1.0.0",
+    "category": "Marketing/Events",
+    "summary": "Add generic and other calendar options to events "
+    "(alongside the existing branded ones)",
+    "author": "Onestein, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/event",
+    "license": "LGPL-3",
+    "depends": [
+        "website_event",
+    ],
+    "data": [
+        "templates/website_event_templates.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_event_calendar/static/src/scss/website.scss",
+        ],
+    },
+}
---
+++ website_event_calendar/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+By default in Odoo only closed source calendars (Google, Outlook, iCal etc.) are shown in the event website view. This module adds also add an generic ICS calendar option alongside some branded open source options.
+
+The iCal and Outlook are using the same ICS format, so the generic calendar option is basically a link to the same ICS file as the Outlook one, but with a different name and icon. This allows to have a more neutral calendar option that can be used in any context, without mentioning specific brands.
