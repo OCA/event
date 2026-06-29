489e5844e566e2c508349a1354659802d00401bc
Cyril VINH-TUNG <cyril@invitu.com>
2026-06-28 16:48:55 -1000

    [ADD] partner_event_gender : use gender for event registration

---
+++ partner_event_gender/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2026 INVITU (<https://www.invitu.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Event Registration Partner Gender",
+    "version": "18.0.1.0.0",
+    "category": "Marketing",
+    "author": "INVITU, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/event",
+    "license": "AGPL-3",
+    "depends": ["partner_event", "partner_contact_gender"],
+    "data": ["views/event_registration_view.xml"],
+    "installable": True,
+}
