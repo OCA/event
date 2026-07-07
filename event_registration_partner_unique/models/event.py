0acda4332f9bb7f5b7dad06730f159038472afd7
Pilar Vargas <pilar.vargas@tecnativa.com>
2026-07-07 15:50:33 +0200

    [MIG] event_registration_partner_unique: Migration to version 19.0
    TT63403

--- event_registration_partner_unique/models/event.py
+++ event_registration_partner_unique/models/event.py
@@ -31,13 +31,13 @@ class EventRegistration(models.Model):
         for event_reg in self.filtered("event_id.forbid_duplicates"):
             dupes = self.search(event_reg._duplicate_search_domain())
             if dupes:
-                # pylint: disable=W8120
                 raise ValidationError(
-                    self.env._("Duplicated partners found in event {0}: {1}.").format(
-                        event_reg.event_id.display_name,
-                        ", ".join(
-                            partner_id.display_name
-                            for partner_id in dupes.mapped("attendee_partner_id")
+                    self.env._(
+                        "Duplicated partners found in event %(event)s: %(partners)s.",
+                        event=event_reg.event_id.display_name,
+                        partners=", ".join(
+                            partner.display_name
+                            for partner in dupes.mapped("attendee_partner_id")
                         ),
                     )
                 )
