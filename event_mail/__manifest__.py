19d3fbbee813db0ab35b744ec59e4b82894b2e44
Andrii Chychkan <mywork1728@gmail.com>
2026-08-18 23:36:46 +0300

    [IMP] event_mail: pre-commit auto fixes

--- event_mail/__manifest__.py
+++ event_mail/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Event Mail",
     "summary": "Mail settings in events",
     "version": "18.0.1.0.0",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/event",
     "category": "Marketing",
